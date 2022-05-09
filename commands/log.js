a5c07f4ea311b784b48700a83db4c433c35c135c
kattah <70865282+kattah7@users.noreply.github.com>
2022-05-09 13:40:32 +0900

    message

--- commands/log.js
+++ commands/log.js
@@ -1,13 +1,17 @@
 const got = require("got");
 
-module.exports = {  
+module.exports = {
     name: "log",
     aliases: [],
     cooldown: 3000,
     execute: async (message, args) => {
         const targetUser = args[0] ?? message.senderUsername;
-        const targetChannel = args[1] ?? message.channelName
-        let { body: userData, statusCode } = await got(`https://api.ivr.fi/twitch/emotes/${args[0]}`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
+        const token = {
+            url: "https://api.twitch.tv/helix/streams?finrst=20",
+        header: {"client-ID": "gp762nuuoqcoxypju8c569th9wz7q5"}
+        }
+        let { body: userData, statusCode } = await got(`${token}`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
         console.log(userData)
-    }
+
+    },
 };
