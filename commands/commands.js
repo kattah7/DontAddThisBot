bc17401f0662b039fcd587f8865951fcd00bb2d5
kattah <70865282+kattah7@users.noreply.github.com>
2022-05-09 11:35:51 +0900

    message

--- commands/commands.js
+++ commands/commands.js
@@ -4,14 +4,8 @@ module.exports = {
     cooldown: 3000,
     execute: async (message, args) => {
         return {
-<<<<<<< HEAD
             text: `${message.senderUsername} WutFace https://pastebin.com/W0shXyxh`,
         };
     },
 };
-=======
-            text: `${message.senderUsername} WutFace https://pastebin.com/taYKTULA`,
-        };
-    },
-};
->>>>>>> 6932fa7 (message)
+
