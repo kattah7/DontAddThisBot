5f9e6e3ede7a61b2a7cd62fbea23e55283c9c953
kattah <kattahpro@gmail.com>
2022-05-10 03:12:39 +0900

    message

--- commands/log.js
+++ commands/log.js
@@ -5,13 +5,13 @@ module.exports = {
     aliases: [],
     cooldown: 3000,
     execute: async (message, args) => {
-        const targetUser = args[0] ?? message.senderUsername;
-        const token = {
-            url: "https://api.twitch.tv/helix/streams?finrst=20",
-        header: {"client-ID": "gp762nuuoqco"}
-        }
-        let { body: userData, statusCode } = await got(`${token}`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
-        console.log(userData)
+        const { data } = await got(`https://api.twitch.tv/helix/clips?broadcaster_id=22484632`, {
+            headers: {
+                "Client-ID": process.env.CLIENT_ID,
+                Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+            },
+        }).json();
 
+        console.log(data);
     },
 };
