9ac487afe3556614fa3bea764bbcb059f9176d11
kattah7 <70865282+kattah7@users.noreply.github.com>
2022-05-09 10:43:16 +0900

    Add files via upload

---
+++ commands/lastseen.js
@@ -0,0 +1,19 @@
+const got = require("got");
+
+module.exports = {  
+    name: "lastseen",
+    aliases: [],
+    cooldown: 3000,
+    execute: async (message, args) => {
+        const targetUser = args[0] ?? message.senderUsername;
+        const targetChannel = args[1] ?? message.channelName
+        let { body: userData, statusCode } = await got(`https://api.ivr.fi/logs/lastmessage/${targetChannel}/${targetUser}`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
+        console.log(userData)
+
+        const lastseen = (userData.time) 
+        return {
+            text: `${targetUser} was last seen ${lastseen} ago. BatChest`
+        }
+
+    }
+};
---
+++ commands/randomline.js
@@ -0,0 +1,20 @@
+const got = require("got");
+
+module.exports = {  
+    name: "randomline",
+    aliases: [],
+    cooldown: 3000,
+    execute: async (message, args) => {
+        const targetUser = args[0] ?? message.senderUsername;
+        const targetChannel = args[1] ?? message.channelName
+        let { body: userData, statusCode } = await got(`https://api.ivr.fi/logs/rq/${targetChannel}/${targetUser}`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
+        console.log(userData)
+
+        const date = (userData.time)
+        const randomline = (userData.message)
+        return {
+            text: `${targetUser} said " ${randomline} " ${date} ago in ${targetChannel}`
+        }
+
+    }
+};
