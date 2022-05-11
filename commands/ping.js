02c1e1b5aec5584de2734669d09dd160bef2236e
kattah <70865282+kattah7@users.noreply.github.com>
2022-05-11 13:30:06 +0900

    message

--- commands/ping.js
+++ commands/ping.js
@@ -10,7 +10,7 @@ module.exports = {
        
 
         return {
-            text: `${message.senderUsername}, PotFriend BOT UPTIME: ${humanizeDuration(process.uptime() * 1000 )}`,
+            text: `${message.senderUsername}, PONG! PotFriend BOT UPTIME: ${humanizeDuration(process.uptime() * 1000 )}`,
         };
     },
 };
---
+++ commands/spam.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "spam",
+    aliases: [],
+    cooldown: 30,
+    execute: async (message, args, client) => {
+        if (args.length < 2) return { text: "Usage: |spam 3 xd" }
+
+        const count = args[0]
+        const phrase = args.slice(1).join(' ').replace('!', 'ǃ').replace('=', '꓿').replace('$', '💲')
+        if (isNaN(count)) return { text: `the spam count should be a number` }
+        if (count > 40) return { text: `the maximum spam count is 100`}
+        if (count < 2) return { text: `the minimum spam count is 2`}
+
+        for (let xd = 0; xd < count; xd++) {
+            client.say(message.channelName, phrase)
+        }
+    },
+};
