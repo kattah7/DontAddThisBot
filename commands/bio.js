2f48f13f23f12eba0ac2c53a4721f1885a6f9f05
kattah <kattahpro@gmail.com>
2022-05-13 20:10:53 +0900

    KEKW

--- commands/bio.js
+++ commands/bio.js
@@ -8,7 +8,7 @@ module.exports = {
     execute: async (message, args) => {
         const targetUser = args[0] ?? message.senderUsername;
         let { body: userData, statusCode } = await got(`https://api.ivr.fi/twitch/resolve/${targetUser}`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
-
+        console.log(userData)
         const bio = userData.bio;
         return {
             text: `${message.senderUsername}, ${bio} PogBones`,
