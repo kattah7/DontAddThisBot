1ac6bf0945f56dc7249d102e336d293fcc226102
kattah <70865282+kattah7@users.noreply.github.com>
2022-05-13 11:48:39 +0900

    xd

---
+++ commands/log.js
@@ -0,0 +1,13 @@
+const got = require("got");
+
+module.exports = {
+    name: "test",
+    aliases: [],
+    cooldown: 3000,
+    description: "test command",
+    execute: async (message, args) => {
+        const targetUser = args[0] ?? message.senderUsername;
+        let { body: userData, statusCode } = await got(`https://recent-messages.robotty.de/api/v2/recent-messages/kattah?limit=5`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
+        console.log(userData)
+    }
+};
