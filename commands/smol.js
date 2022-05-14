d358a7b6b350762c0c06b27395c6c45bbd37d468
kattah <kattahpro@gmail.com>
2022-05-14 13:47:51 +0900

    yo

---
+++ commands/smol.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "smol",
+    aliases: [],
+    cooldown: 3000,
+    description:"smol emotes",
+    execute: async (message, args) => {
+         
+            return {
+                text: 'Okaygi peepoShi Comfi Madgi Madgi Praygi 7Homis Donki Homi Pepegi Peepi peepiWTF Weirdgi pepegaSitty Susgi Apugi Strongi Painsgi '
+            }
+        
+    },
+};
