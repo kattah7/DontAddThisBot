fa888f83d86d0dbad49a363f2426eafdc1d596f8
kattah <kattahpro@gmail.com>
2022-05-15 00:52:03 +0900

    yo

--- commands/chatstats.js
+++ commands/chatstats.js
@@ -11,8 +11,14 @@ module.exports = {
         let userData = await got(`https://api.streamelements.com/kappa/v2/chatstats/${targetUser}/stats`, { timeout: 10000}).json();
         console.log(userData);
         
-        return {
-            text: `Total ${userData.totalMessages} MESSAGES, Top Chatter: ${userData.chatters[0].name} Amount: ${userData.chatters[0].amount}, Top BTTV Emote: ${userData.bttvEmotes[0].emote} used ${userData.bttvEmotes[0].amount} times, Top FFZ Emote: ${userData.ffzEmotes[0].emote} used ${userData.ffzEmotes[0].amount}, Top TWITCH Emote: ${userData.twitchEmotes[0].emote} used ${userData.twitchEmotes[0].amount}`
+        if (userData.code == 'ERR_NON_2XX_3XX_RESPONSE') {
+            return {
+                text: `Channel doesn't exist :p maybe try using old name?`
+            }
+        } else {
+            return {
+                text: `Total ${userData.totalMessages} MESSAGES, Top Chatter: ${userData.chatters[0].name} Amount: ${userData.chatters[0].amount}, Top BTTV Emote: ${userData.bttvEmotes[0].emote} used ${userData.bttvEmotes[0].amount} times, Top FFZ Emote: ${userData.ffzEmotes[0].emote} used ${userData.ffzEmotes[0].amount}, Top TWITCH Emote: ${userData.twitchEmotes[0].emote} used ${userData.twitchEmotes[0].amount}`
+            } 
         }
         
     },
