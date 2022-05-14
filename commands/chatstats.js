39d50147cb4efc4e28d25706428735f2f4784412
kattah <kattahpro@gmail.com>
2022-05-14 23:56:44 +0900

    message

--- commands/chatstats.js
+++ commands/chatstats.js
@@ -12,7 +12,7 @@ module.exports = {
         console.log(userData);
         
         return {
-            text: `This channel has total of ${userData.totalMessages} messages`
+            text: `Total ${userData.totalMessages} MESSAGES, Top Chatter: ${userData.chatters[0].name} Amount: ${userData.chatters[0].amount}, Top BTTV Emote: ${userData.bttvEmotes[0].emote} used ${userData.bttvEmotes[0].amount} times, Top FFZ Emote: ${userData.ffzEmotes[0].emote} used ${userData.ffzEmotes[0].amount}, Top TWITCH Emote: ${userData.twitchEmotes[0].emote} used ${userData.twitchEmotes[0].amount}`
         }
         
     },
