dc832e88ee6a0bec3b69decf7559ae684ba33e3f
Fuchsty <fuchsty@gmail.com>
2022-05-16 00:52:51 +0200

    asd

--- util/db.js
+++ util/db.js
@@ -1,6 +1,6 @@
 const DB = require("mongoose");
 
-DB.connect(`mongodb://localhost:27017/dontaddthisbot`, {});
+DB.connect(`mongodb://127.0.0.1:27017/dontaddthisbot`, {});
 
 DB.connection.on("connected", () => {
     console.log(`Connected to database!`);
