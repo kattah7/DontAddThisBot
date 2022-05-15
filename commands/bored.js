147e4630b6d83a6069c7388a470cf4d0872b23bb
kattah <kattahpro@gmail.com>
2022-05-16 05:34:49 +0900

    yo

--- commands/bored.js
+++ commands/bored.js
@@ -5,23 +5,24 @@ module.exports = {
     name: "bored",
     aliases: [],
     cooldown: 3000,
-    description:"Get stuff to do every 12 hours",
+    description: "Get stuff to do every 12 hours",
     execute: async (message, args) => {
         const lastUsage = await bot.Redis.get(`test:${message.senderUsername}`);
         const targetUser = args[0] ?? message.senderUsername;
         let { body: userData, statusCode } = await got(`https://www.boredapi.com/api/activity?participants=1`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
-        console.log(userData)
+        console.log(userData);
 
         if (lastUsage) {
-            if (new Date().getTime()  - new Date(lastUsage).getTime() < 1000 * 60 * 60 * 12) {
+            if (new Date().getTime() - new Date(lastUsage).getTime() < 1000 * 60 * 60 * 12) {
+                const ms = new Date().getTime() + 1000 * 60 * 60 * 12 - new Date(lastUsage).getTime();
                 return {
-                    text: `This command can only be used every 12hours. Please wait ${43200 - ((Math.round(new Date().getTime() - new Date(lastUsage).getTime()) / 1000))}s.`,
+                    text: `This command can only be used every 12hours. Please wait ${humanizeDuration(ms)}s.`,
                 };
             }
         }
         await bot.Redis.set(`test:${message.senderUsername}`, Date.now(), 0);
         return {
-            text: `${targetUser}, ${userData.activity} `
-        }
+            text: `${targetUser}, ${userData.activity} `,
+        };
     },
-}
+};
