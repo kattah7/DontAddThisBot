91c1cc518d3ddb5fba0fbdb806d10ad9a873d1bd
Fuchsty <fuchsty@gmail.com>
2022-05-15 20:20:41 +0200

    asd

--- commands/test.js
+++ commands/test.js
@@ -7,7 +7,7 @@ module.exports = {
         const lastUsage = await bot.Redis.get(`test:${message.senderUsername}`);
 
         if (lastUsage) {
-            if (Date.now() - new Date(lastUsage).getTime() > 1000 * 60 * 5) {
+            if (Date.now() - new Date(lastUsage).getTime() < 1000 * 60 * 5) {
                 return {
                     text: `This command can only be used every 5 minutes. Please wait ${Date.now() - new Date(lastUsage).getTime() * 1000}s before trying to use it again.`,
                 };
