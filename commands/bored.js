51c16eb438752df4b5639f491573ae5b8a26f6a4
kattah <kattahpro@gmail.com>
2022-05-16 04:13:59 +0900

    lol

--- commands/bored.js
+++ commands/bored.js
@@ -6,19 +6,21 @@ module.exports = {
     cooldown: 3000,
     description:"Get stuff to do every 12 hours",
     execute: async (message, args) => {
-        await bot.Redis.set(`cookie:${message.senderUsername}`, Date.now());
+        const lastUsage = await bot.Redis.get(`test:${message.senderUsername}`);
         const targetUser = args[0] ?? message.senderUsername;
         let { body: userData, statusCode } = await got(`https://www.boredapi.com/api/activity?participants=1`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
         console.log(userData)
 
-        if (await bot.Redis.set(`cookie:${message.senderUsername}`, Date.now() < 12*60*60)) {
-            return {
-                text: `${targetUser}, ${userData.activity}`
-            } 
-        } else if (await bot.Redis.set(`cookie:${message.senderUsername}`, Date.now() > 12*60*60)) {
-            return {
-                text: `wait 12 hours`
+        if (lastUsage) {
+            if (Date.now() - new Date(lastUsage).getTime() < 1000 * 60 * 60 * 12) {
+                return {
+                    text: `This command can only be used every 12hours. Please wait ${Date.now() - new Date(lastUsage).getTime() / 1000}s.`,
+                };
             }
+        }
+        await bot.Redis.set(`test:${message.senderUsername}`, Date.now());
+        return {
+            text: `${targetUser}, ${userData.activity} `
         }
     },
 }
--- commands/test.js
+++ commands/test.js
@@ -1,23 +1,12 @@
+const got = require("got");
+
 module.exports = {
     name: "test",
     aliases: [],
     cooldown: 3000,
     description: "lol",
     execute: async (message, args) => {
-        const lastUsage = await bot.Redis.get(`test:${message.senderUsername}`);
-
-        if (lastUsage) {
-            if (Date.now() - new Date(lastUsage).getTime() < 1000 * 60 * 5) {
-                return {
-                    text: `This command can only be used every 5 minutes. Please wait ${Date.now() - new Date(lastUsage).getTime() / 1000}s before trying to use it again.`,
-                };
-            }
-        }
-
-        await bot.Redis.set(`test:${message.senderUsername}`, Date.now());
-
-        return {
-            text: `Lol this worked`,
-        };
+        let { body: userData, statusCode } = await got(`https://www.boredapi.com/api/activity?participants=1`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
+        console.log(userData)
     },
 };
