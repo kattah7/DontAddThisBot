ebf91b21e389670db95b6baa1edba60e5c610a64
kattah <kattahpro@gmail.com>
2022-05-17 02:48:37 +0900

    yo

--- commands/kekw.js
+++ commands/kekw.js
@@ -4,14 +4,17 @@ module.exports = {
     name: "kekw",
     aliases: [],
     cooldown: 3000,
+    description: "lol",
     execute: async (message, args) => {
         const subArray = [];
         const { chatters } = await got(`https://tmi.twitch.tv/group/user/${message.channelName}/chatters`).json();
+        console.log(chatters)
         for (const user of [...chatters.broadcaster, ...chatters.moderators, ...chatters.vips, ...chatters.viewers]) {
-            const { hidden, subscribed } = await got(`https://api.ivr.fi/twitch/subage/${encodeURIComponent(user)}/${args[0]}`, { throwHttpErrors: false, timeout: 3000 }).json();
+            const { hidden, subscribed } = await got(`https://api.ivr.fi/twitch/subage/${encodeURIComponent(user)}/${message.channelName}}`, { throwHttpErrors: false, timeout: 3000 }).json();
             if (!hidden && subscribed) subArray.push(user);
+            console.log(subscribed)
         }
 
-        return { text: `Out of ${chattersArray.length} users, ${subArray.length} are subscribed to zoil 😴 (${subArray.join(", ")})` };
+        return { text: `Out of ${[...chatters.broadcaster, ...chatters.moderators, ...chatters.vips, ...chatters.viewers].length} users, ${subArray.length} are subscribed to ${message.channelName} 😴 (${subArray.join(", ")})` };
     },
 };
