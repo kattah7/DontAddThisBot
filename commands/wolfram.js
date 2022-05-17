3e7ccf7c13a6da9db9a9e6c299f84dd65d1563d3
kattah <kattahpro@gmail.com>
2022-05-17 13:49:22 +0900

    yo

---
+++ commands/wolfram.js
@@ -0,0 +1,17 @@
+const got = require("got");
+
+module.exports = {
+    name: "wolfram",
+    aliases: [],
+    cooldown: 3000,
+    description:"search anything and get a answer",
+    execute: async (message, args) => {
+        let { body: userData, statusCode } = await got(`https://api.wolframalpha.com/v1/conversation.jsp?appid=${process.env.WOLFRAM_APP_ID}&i=${encodeURIComponent(args.join(" "))}`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
+        console.log(userData)
+        
+        return {
+            text: `${message.senderUsername}, ${userData.result}`
+        }
+    },
+    
+};
