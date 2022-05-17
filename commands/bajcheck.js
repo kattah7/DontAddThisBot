2f345682f74450c71e4ee9698ae75dff0b2ebce3
kattah <kattahpro@gmail.com>
2022-05-17 17:14:39 +0900

    yo

---
+++ commands/bajcheck.js
@@ -0,0 +1,47 @@
+const got = require("got");
+const humanizeDuration = require("../humanizeDuration");
+
+module.exports = {
+    name: "bajcheck",
+    cooldown: 3000,
+    description: "checks if a user is a baj",
+
+    execute: async(message, args) => {
+        const USERNAME = args[0] ?? message.senderUsername
+        let data = await got(`https://api.ivr.fi/twitch/subage/${USERNAME}/forsen`, { timeout: 10000}).json();
+        console.log(data)
+
+        const followAge = new Date().getTime() - Date.parse(data.followedAt);
+        
+        if (data) {
+            if (data.followedAt == null) {
+                if (data.streak.months == 0) {
+                    return {
+                        text: `${data.username} WAS NEVER SUBBED & FOLLOWING WutFace`
+                    }
+                } else  {
+                    return {
+                        text: `${data.username} is subbed to forsen for ${data.cumulative.months} months & not following. Susgi`
+                    }
+                }
+            } else if (data.cumulative.months == 0) {
+                if (data.cumulative.months == 0) {
+                    return {
+                        text: `${data.username} was never subbed to forsen & following for ${humanizeDuration(followAge)} Susgi`
+                    } 
+                }
+            } else if (data.cumulative.months > 0) {
+                if (data.subscribed == false) {
+                    return {
+                        text: `${data.username} was previously subbed to forsen for ${data.cumulative.months} months & following for ${humanizeDuration(followAge)} Susgi`
+                    }
+                } else if (data.subscribed == true) {
+                    return {
+                        text: `${data.username} is subbed to forsen for ${data.cumulative.months} months & following for ${humanizeDuration(followAge)} Susgi`
+                    }
+                }
+            }
+        }
+    }
+
+}
