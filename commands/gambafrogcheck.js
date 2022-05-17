ea6474a3b00d59aeee0a258248694c4b919f3862
kattah <kattahpro@gmail.com>
2022-05-17 17:44:43 +0900

    yo

---
+++ commands/gambafrogcheck.js
@@ -0,0 +1,52 @@
+const got = require("got");
+const humanizeDuration = require("../humanizeDuration");
+
+module.exports = {
+    name: "gambafrog",
+    cooldown: 3000,
+    description: "checks if a user is a gambafrog in trainwreckstv",
+
+    execute: async(message, args) => {
+        const USERNAME = args[0] ?? message.senderUsername
+        let data = await got(`https://api.ivr.fi/twitch/subage/${USERNAME}/trainwreckstv`, { timeout: 10000}).json();
+        console.log(data)
+
+        const followAge = new Date().getTime() - Date.parse(data.followedAt);
+        
+        if (data) {
+            if (data.followedAt == null) {
+                if (data.streak.months == 0) {
+                    return {
+                        text: `${data.username} WAS NEVER SUBBED & FOLLOWING 🦍`
+                    }
+                } else  {
+                    return {
+                        text: `${data.username} is subbed to domey for ${data.cumulative.months} months & not following 🦍`
+                    }
+                }
+            } else if (data.cumulative.months == 0) {
+                if (data.cumulative.months == 0) {
+                    return {
+                        text: `${data.username} was never subbed to domey & following for ${humanizeDuration(followAge)} 🦍`
+                    } 
+                } 
+            } else if (data.cumulative.months > 0) {
+                if (data.subscribed == false) {
+                    return {
+                        text: `${data.username} was previously subbed to domey for ${data.cumulative.months} months & following for ${humanizeDuration(followAge)} 🦍`
+                    }
+                } else if (data.subscribed == true) {
+                    if (data.cumulative.months == 1) {
+                        return {
+                            text: `WutFace GAMBA FROG`
+                        }
+                    }
+                    return {
+                        text: `${data.username} is subbed to domey for ${data.cumulative.months} months & following for ${humanizeDuration(followAge)} 🦍`
+                    }
+                }
+            } 
+        } 
+    } 
+
+}
