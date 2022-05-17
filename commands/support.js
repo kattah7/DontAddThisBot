26b59a6c3ef14daa59f6189fdb6c6d3e5848aaf7
kattah <kattahpro@gmail.com>
2022-05-17 23:22:07 +0900

    yo

---
+++ commands/support.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "support",
+    aliases: ["donate"],
+    cooldown: 3000,
+    description:"If you would like to help my monthly server cost TriHard",
+    execute: async (message, args) => {
+        const targetUser = args[0] ?? message.senderUsername;
+            return {
+                text: `${targetUser}, https://ko-fi.com/kattah ☕ `
+            }
+        
+    },
+};
