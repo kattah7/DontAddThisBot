ac88aaa16b9089a989db3acf5c3ff8c664163ca6
kattah <kattahpro@gmail.com>
2022-05-20 03:02:23 +0900

    yo

--- commands/juicercheck.js
+++ commands/juicercheck.js
@@ -8,7 +8,7 @@ module.exports = {
 
     execute: async(message, args) => {
         const USERNAME = args[0] ?? message.senderUsername
-        let data = await got(`https://api.ivr.fi/twitch/subage/${USERNAME}/xqcow`, { timeout: 10000}).json();
+        let data = await got(`https://api.ivr.fi/twitch/subage/${USERNAME}/xqc`, { timeout: 10000}).json();
         console.log(data)
 
         const followAge = new Date().getTime() - Date.parse(data.followedAt);
@@ -21,23 +21,23 @@ module.exports = {
                     }
                 } else  {
                     return {
-                        text: `${data.username} is subbed to xQcOW for ${data.cumulative.months} months & not following. Susgi`
+                        text: `${data.username} is subbed to xQc for ${data.cumulative.months} months & not following. xqcL`
                     }
                 }
             } else if (data.cumulative.months == 0) {
                 if (data.cumulative.months == 0) {
                     return {
-                        text: `${data.username} was never subbed to xQcOW & following for ${humanizeDuration(followAge)} Susgi`
+                        text: `${data.username} was never subbed to xQc & following for ${humanizeDuration(followAge)} xqcL`
                     } 
                 }
             } else if (data.cumulative.months > 0) {
                 if (data.subscribed == false) {
                     return {
-                        text: `${data.username} was previously subbed to xQcOw for ${data.cumulative.months} months & following for ${humanizeDuration(followAge)} Susgi`
+                        text: `${data.username} was previously subbed to xQc for ${data.cumulative.months} months & following for ${humanizeDuration(followAge)} xqcL`
                     }
                 } else if (data.subscribed == true) {
                     return {
-                        text: `${data.username} is subbed to xQcOW for ${data.cumulative.months} months & following for ${humanizeDuration(followAge)} Susgi`
+                        text: `${data.username} is subbed to xQc for ${data.cumulative.months} months & following for ${humanizeDuration(followAge)} xqcL`
                     }
                 }
             }
