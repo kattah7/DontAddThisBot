b2b96260464c8b00a07ecd8243e2dd6aecb0fcc2
kattah <kattahpro@gmail.com>
2022-05-20 01:42:16 +0900

    yo

--- commands/chatters.js
+++ commands/chatters.js
@@ -6,7 +6,7 @@ module.exports = {
         const targetUser = args[0] ?? message.senderUsername;
 
 
-  const data = await fetch(`https://recent-messages.robotty.de/api/v2/recent-messages/${targetUser}`);
+  const data = await fetch(`https://recent-messages.robotty.de/api/v2/recent-messages/${targetUser.toLowerCase()}`);
   const resp = JSON.parse(await data.text());
 
   const messages = resp.messages;
