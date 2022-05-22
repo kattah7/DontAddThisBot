1b54b408ae3f09bfadfb77fcdfb4a528e3e26c0b
kattah <kattahpro@gmail.com>
2022-05-22 12:54:48 +0900

    yo

--- commands/coins.js
+++ commands/coins.js
@@ -3,7 +3,7 @@ const humanizeDuration = require("../humanizeDuration");
 module.exports = {
     name: "poro",
     cooldown: 3000,
-    description: "Get poro every 1 hour",
+    description: "Get poro meat every 1 hour",
     execute: async(message, args) => {
         const lastUsage = await bot.Redis.get(`poro:${message.senderUsername}`);
         const channelData = await bot.DB.poroCount.findOne({ username: message.senderUsername }).exec();
@@ -29,7 +29,7 @@ module.exports = {
             if (new Date().getTime() - new Date(lastUsage).getTime() < 1000 * 60 * 60 * 1) {
                 const ms = new Date(lastUsage).getTime() - new Date().getTime() + 1000 * 60 * 60 *1;
                 return {
-                    text: `Come back in 1 hour PoroRoast Please wait ${humanizeDuration(ms)}. PoroMeat ${channelData.poroCount} meat total!`,
+                    text: `No poros found... 🌉 PoroSad | ${channelData.poroCount} meat total! 🥩  | Come back later in ${humanizeDuration(ms)}.`,
                 };
             }
         }
