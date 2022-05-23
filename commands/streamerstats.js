f6dea72ba39978486ce42c6d92c18cecbd2de9ff
kattah <kattahpro@gmail.com>
2022-05-24 04:54:56 +0900

    yo

--- commands/streamerstats.js
+++ commands/streamerstats.js
@@ -7,13 +7,13 @@ module.exports = {
     description:"check streamer rank in the past 30 days",
     execute: async (message, args) => {
         const targetChannel = args[0] ?? message.channelName
-        let { body: userData, statusCode } = await got(`https://twitchtracker.com/api/channels/summary/xqc`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
-        const myArr = JSON.parse(JSON.stringify(userData))
-
-        console.log(myArr)
-
-       return {
-           text: `${myArr.rank}`
-       }
+        const { body: pogger, statusCode2 } = await got.get(`https://twitchtracker.com/api/channels/summary/xqc`, {
+            throwHttpErrors: false,
+            responseType: 'json',
+        })
+        console.log(pogger)
+        return {
+            text: `${pogger.rank}`
+        }
     },
 };
