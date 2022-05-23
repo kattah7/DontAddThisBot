481cf6f151d2287ae86ab84050c245213b366f6d
kattah <kattahpro@gmail.com>
2022-05-24 04:34:42 +0900

    yo

--- commands/streamerstats.js
+++ commands/streamerstats.js
@@ -1,4 +1,4 @@
-const got = require("got");
+const fetch = require("../node_modules/node-fetch")
 
 module.exports = {
     name: "stats",
@@ -7,11 +7,14 @@ module.exports = {
     description:"check streamer rank in the past 30 days",
     execute: async (message, args) => {
         const targetChannel = args[0] ?? message.channelName
-        const { body: userData, statusCode } = await got(`https://twitchtracker.com/api/channels/summary/${targetChannel.toLowerCase()}`, { timeout: 10000, throwHttpErrors: true, responseType: "json" });
-        console.log(userData)
+        const response = await fetch('https://twitchtracker.com/api/channels/summary/xqc');
+        const body = await response.json();
 
-       return {
-        text: `Data for ${targetChannel} in 30 Days, RANK: ${userData.rank} | Streamed: ${userData.minutes_streamed} minutes | Hours Watched: ${userData.hours_watched} Hours | AVG: ${userData.avg_viewers} PEAK: ${userData.max_viewers} Viewers`
-       }
+        console.log(body); 
+        return {
+            text: `${body.rank}`
+        }
+
+       
     },
 };
