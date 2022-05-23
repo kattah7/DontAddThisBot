2a6053002ca723c8055bd661bd5b0f756d1e6219
kattah <kattahpro@gmail.com>
2022-05-24 06:05:38 +0900

    yo

--- cloudfare.js
+++ cloudfare.js
@@ -1,17 +1,8 @@
-const puppeteer = require('puppeteer');
+const CloudflareBypasser = require('cloudflare-bypasser');
 
-(async function main() {
-    try {
-        const browser = await puppeteer.launch();
-        const [page] = await browser.pages();
+let cf = new CloudflareBypasser();
 
-        await page.goto('https://twitchtracker.com/api/channels/summary/xqc', { waitUntil: 'networkidle0' });
-        const bodyHTML = await page.evaluate(() => document.body.innerHTML);
-
-        console.log(bodyHTML);
-
-        await browser.close();
-    } catch (err) {
-        console.error(err);
-    }
-})();
+cf.request('https://twitchtracker.com/api/channels/summary/xqc')
+.then(res => {
+  console.log(res)
+});
--- commands/streamerstats.js
+++ commands/streamerstats.js
@@ -1,6 +1,5 @@
-const got = require("got");
-const cloudflareScraper = require('cloudflare-scraper');
-const puppeteer = require('puppeteer');
+const CloudflareBypasser = require('cloudflare-bypasser');
+const got = require("got")
 
 module.exports = {
     name: "stats",
@@ -8,20 +7,12 @@ module.exports = {
     cooldown: 3000,
     description:"check streamer rank in the past 30 days",
     execute: async (message, args) => {
-        (async function main() {
-            try {
-                const browser = await puppeteer.launch();
-                const [page] = await browser.pages();
-        
-                await page.goto('https://twitchtracker.com/api/channels/summary/xqc', { waitUntil: 'networkidle0' });
-                const bodyHTML = await page.evaluate(() => document.body.innerHTML);
-        
-                console.log(bodyHTML);
-        
-                await browser.close();
-            } catch (err) {
-                console.error(err);
-            }
-        })();
+        let cf = new CloudflareBypasser();
+
+        cf.request('https://twitchtracker.com/api/channels/summary/xqc')
+        .then(res => {
+        console.log(res)
+        });
+
     },
 };
