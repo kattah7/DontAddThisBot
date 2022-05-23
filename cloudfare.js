4e6b83d15b165e228b5f5736b0fb78b2b530a647
kattah <kattahpro@gmail.com>
2022-05-24 06:09:06 +0900

    yo

--- cloudfare.js
+++
@@ -1,8 +0,0 @@
-const CloudflareBypasser = require('cloudflare-bypasser');
-
-let cf = new CloudflareBypasser();
-
-cf.request('https://twitchtracker.com/api/channels/summary/xqc')
-.then(res => {
-  console.log(res)
-});
