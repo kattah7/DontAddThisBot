b9584304f0a7647e02cb3476ea86c4228cd00fa0
kattah <kattahpro@gmail.com>
2022-05-23 16:27:32 +0900

    yo

--- commands/7tvprofile.js
+++ commands/7tvprofile.js
@@ -1,13 +1,27 @@
+const got = require("got")
+
 module.exports = {
-    name: "7tv",
-    aliases: [""],
-    cooldown: 3000,
-    description:"Grabs 7tv profile of user",
-    execute: async (message, args) => {
-        if (args.length < 1) return { text: "Use |7tv (name)" };
-        
+  name: "7tv",
+  cooldown: 1000,
+  execute: async(message, args) => {
+
+    const targetUser = args[0] ?? message.senderUsername
+    let { body: userData, statusCode } = await got(`https://api.7tv.app/v2/users/${targetUser.toLowerCase()}`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
+        console.log(userData)
+
+    const { body: pogger } = await got.post(`https://api.7tv.app/v2/gql`, {
+            throwHttpErrors: false,
+            responseType: 'json',
+            json: {
+                "query": "query GetUser($id: String!) {user(id: $id) {...FullUser,, banned, youtube_id}}fragment FullUser on User {id,email, display_name, login,description,role {id,name,position,color,allowed,denied},emote_aliases,emotes { id, name, status, visibility, width, height },owned_emotes { id, name, status, visibility, width, height },emote_ids,editor_ids,editors {id, display_name, login,role { id, name, position, color, allowed, denied },profile_image_url,emote_ids},editor_in {id, display_name, login,role { id, name, position, color, allowed, denied },profile_image_url,emote_ids},follower_count,broadcast {type,title,game_name,viewer_count,},twitch_id,broadcaster_type,profile_image_url,created_at,emote_slots,audit_entries {id,type,timestamp,action_user_id,action_user {id, display_name, login,role { id, name, position, color, allowed, denied },profile_image_url,emote_ids},changes {key, values},target {type,data,id},reason}}",
+                "variables": {
+                    "id": userData.id
+                }
+            }
+        })
+        console.log(pogger)
         return {
-            text: `${message.senderUsername}, https://7tv.app/users/${args} YEAHBUT7TV`,
-        };
-    },
-};
+            text: `YEAHBUT7TV 7tvM ${pogger.data.user.display_name}'s Emote Slots ${pogger.data.user.emote_slots}, Created: ${pogger.data.user.created_at.split("T")[0]}, 7tv.app/users/${pogger.data.user.display_name}`
+        }
+  }
+}
