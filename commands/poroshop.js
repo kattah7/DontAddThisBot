a2049d8aa90f7602a0baf2ec4123ef99c4e10f61
kattah <kattahpro@gmail.com>
2022-05-26 01:22:09 +0900

    added poroshop

---
+++ commands/poroshop.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "poroshop",
+    cooldown: 3000,
+    description: "poro shop information to use poro meat",
+    execute: async(message, args) => {
+        return {
+            text: `${message.senderUsername}, kattahDance Change bot color (100 poro 🥩)`
+        }
+    }
+}
