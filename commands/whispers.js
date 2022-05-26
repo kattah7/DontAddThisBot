d93adb3d288462449ec588999af3b05aa5608c01
kattah <kattahpro@gmail.com>
2022-05-27 07:17:20 +0900

    added whispers

---
+++ commands/whispers.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "whispers",
+    aliases: [],
+    cooldown: 3000,
+    description:"whisper popout",
+    execute: async (message, args) => {
+        const targetUser = args[0] ?? message.senderUsername;
+            return {
+                text: `https://www.twitch.tv/popout/moderator/${targetUser}/whispers `
+            }
+        
+    },
+};
