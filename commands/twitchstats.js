d89a5c3269eb4ce4b8e24bcd360cd2ba9878c441
kattah <kattahpro@gmail.com>
2022-05-31 02:14:00 +0900

    added stats

---
+++ commands/twitchstats.js
@@ -0,0 +1,26 @@
+const got = require("got");
+const humanizeDuration = require("../humanizeDuration");
+
+module.exports = {
+    name: "stats",
+    cooldown: 3000,
+    description: "get stats from twitchinsight.net/games",
+    execute: async(message, args, client) => {
+        let { body: userData, statusCode } = await got(`https://api.twitchinsights.net/v1/game/all`, { timeout: 10000, throwHttpErrors: false, responseType: "json" });
+        let xd = 0;
+        let xd2 = 0;
+        console.log(userData)
+        for (const data of userData.games) {
+            try {
+                xd += data.streamers
+                xd2 += data.viewers
+            } catch (err) {
+                console.error(`error`, err);
+            }
+         }
+         const lol = args[0]
+         const ms = new Date(lol).getTime() - new Date().getTime() + 1000 * 60 * 15;
+         return {
+             text: `Currently ${xd.toLocaleString()} streamers are live, ${xd2.toLocaleString()} total viewers on twitch. Total ${userData.total.toLocaleString()} categories OMGScoots`
+         }
+}}
