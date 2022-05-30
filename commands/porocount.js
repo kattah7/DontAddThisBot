cc32150031916851141a77cf7101f8980fef3e17
kattah <kattahpro@gmail.com>
2022-05-30 13:42:41 +0900

    yo

--- commands/porocount.js
+++ commands/porocount.js
@@ -6,7 +6,7 @@ module.exports = {
         const targetUser = args[0] ?? message.senderUsername
         const channelData = await bot.DB.poroCount.findOne({ username: targetUser }).exec();
         
-        if (channelData.poroCount == null) {
+        if (channelData.poroCount) {
             return {
                 text: `doesnt exist lol`
             }
