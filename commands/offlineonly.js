8425e365d523c627acf438aadbde7d96c09cfccd
kattah <kattahpro@gmail.com>
2022-06-02 04:11:48 +0900

    test

---
+++ commands/offlineonly.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "testxd123",
+    cooldown: 3000,
+    description: "Make the bot only type in when channel is offline",
+    permission: 2,
+    level: 3,
+    execute: async(message, args) => {
+        return {
+            text: `xd`
+        }
+    }
+}
