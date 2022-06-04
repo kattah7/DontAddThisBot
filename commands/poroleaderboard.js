e523354e36aafcb8e9ebba5b997abde1b8c88403
kattah <kattahpro@gmail.com>
2022-06-05 08:03:19 +0900

    yo

--- commands/poroleaderboard.js
+++ commands/poroleaderboard.js
@@ -3,13 +3,14 @@ module.exports = {
     cooldown: 10000,
     aliases: ["lb"],
     description: "See leaderboard of poro meat",
+    poro: true,
     execute: async (message, args, client) => {
         const poroData = await bot.DB.poroCount.find({}).exec();
 
         const sorted = poroData.sort((a, b) => b.poroCount - a.poroCount);
 
         const top5 = sorted.slice(0, 5);
-        const top5String = top5.map((user) => `ㅤ${user.username} - ${user.poroCount} `).join(" | ");
+        const top5String = top5.map((user) => `${user.username} - ${user.poroCount} `).join(" | ");
 
         if (message.senderUsername == process.env.NUMBER_ONE) {
             client.privmsg(message.channelName, `.me kattahXd Poro leaderboard: ${top5String}`)
