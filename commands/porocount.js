c9e1b5a5ac59ca7c50acba24f89e2fe6f4b82353
kattah <kattahpro@gmail.com>
2022-06-06 04:05:12 +0900

    yo

--- commands/porocount.js
+++ commands/porocount.js
@@ -7,31 +7,38 @@ module.exports = {
     poro: true,
     execute: async(message, args, client) => {
         const targetUser = args[0] ?? message.senderUsername
+        const prefix = args[0].toLowerCase()
         const {banned, banphrase_data} = await got.post(`https://forsen.tv/api/v1/banphrases/test `, {json: {'message': targetUser}}).json();
         console.log(banned, banphrase_data)
         const channelData = await bot.DB.poroCount.findOne({ username: targetUser.toLowerCase() }).exec();
         
-        if (banned == false) {
-            if (!channelData) {
-                if (message.senderUsername == process.env.NUMBER_ONE) {
-                    client.privmsg(message.channelName, `.me ${targetUser} isnt registered lol`)
-                } else {
-                    return {
-                        text: `${targetUser} isnt registered lol`
+        if (prefix.length < 25) {
+            if (banned == false) {
+                if (!channelData) {
+                    if (message.senderUsername == process.env.NUMBER_ONE) {
+                        client.privmsg(message.channelName, `.me ${targetUser} isnt registered lol`)
+                    } else {
+                        return {
+                            text: `${targetUser} isnt registered lol`
+                        }
                     }
-                }
-            } else {
-                if (message.senderUsername == process.env.NUMBER_ONE) {
-                    client.privmsg(message.channelName, `.me ${targetUser} has total of ${channelData.poroCount} meat kattahXd | Registered: ${channelData.joinedAt}`)
                 } else {
-                    return {
-                        text: `${targetUser} has total of ${channelData.poroCount} meat kattahXd | Registered: ${channelData.joinedAt}`
+                    if (message.senderUsername == process.env.NUMBER_ONE) {
+                        client.privmsg(message.channelName, `.me ${targetUser} has total of ${channelData.poroCount} meat kattahXd | Registered: ${channelData.joinedAt}`)
+                    } else {
+                        return {
+                            text: `${targetUser} has total of ${channelData.poroCount} meat kattahXd | Registered: ${channelData.joinedAt}`
+                        }
                     }
+                } 
+            } else if (banned == true) {
+                return {
+                    text: `banned msg lol`
                 }
-            } 
-        } else if (banned == true) {
+            }
+        } else {
             return {
-                text: `banned msg lol`
+                text: `message too long lol`
             }
         }
     }
