dfa50b3951d44656ca449bb1309230b90dbe2fc1
kattah <kattahpro@gmail.com>
2022-06-06 04:55:53 +0900

    yo

--- commands/randomstream.js
+++ commands/randomstream.js
@@ -2,11 +2,16 @@ const got = require("got");
 const humanizeDuration = require("../humanizeDuration");
 
 module.exports = {
-    name: "randomstream",
+    name: "testxd",
     aliases: ["rs"],
     cooldown: 3000,
     description:"Fetches a random streamer from any game category",
     execute: async (message, args, client) => {
+        if (!args[0]) {
+            return {
+                text: `Pls write a category lol`
+            } 
+        }
         
 
         const { data } = await got(`https://api.twitch.tv/helix/games?name=${args.join(" ")}`, {
@@ -22,16 +27,18 @@ module.exports = {
                 Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
             },
             }).json();
-            console.log(data2)
-            const random = Math.floor(Math.random() * 100) + 0;
-            const ms = new Date().getTime() - Date.parse(data2[random].started_at);
-            if (message.senderUsername == process.env.NUMBER_ONE) {
-                client.privmsg(message.channelName, `.me ${data2[random].user_name} been live for ${humanizeDuration(ms)} playing ${data2[random].game_name} with ${data2[random].viewer_count} viewers. Title: ${data2[random].title}. twitch.tv/${data2[random].user_login}`)
-            } else {
-                return {
-                    text: `${data2[random].user_name} been live for ${humanizeDuration(ms)} playing ${data2[random].game_name} with ${data2[random].viewer_count} viewers. Title: ${data2[random].title}. twitch.tv/${data2[random].user_login}`
+            var random = data2[Math.floor(Math.random()*data2.length)];
+            const ms = new Date().getTime() - Date.parse(random.started_at);
+            if (args[0].toLowerCase()) {
+                if (message.senderUsername == process.env.NUMBER_ONE) {
+                    client.privmsg(message.channelName, `.me ${random.user_name} been live for ${humanizeDuration(ms)} playing ${random.game_name} with ${random.viewer_count} viewers. Title: ${random.title}. twitch.tv/${random.user_login}`)
+                } else {
+                    return {
+                        text: `${random.user_name} been live for ${humanizeDuration(ms)} playing ${random.game_name} with ${random.viewer_count} viewers. Title: ${random.title}. twitch.tv/${random.user_login}`
+                    }
                 }
             }
+            
 
     },
 };
