e10beb5b2ac90276629408e1094764e6e3ff0130
kattah <kattahpro@gmail.com>
2022-06-06 21:03:50 +0900

    yo

--- commands/resize.js
+++ commands/resize.js
@@ -11,7 +11,7 @@ module.exports = {
             json: {
                 "query": "query GetUser($id: String!) {user(id: $id) {...FullUser,, banned, youtube_id}}fragment FullUser on User {id,email, display_name, login,description,role {id,name,position,color,allowed,denied},emote_aliases,emotes { id, name, status, visibility, width, height },owned_emotes { id, name, status, visibility, width, height },emote_ids,editor_ids,editors {id, display_name, login,role { id, name, position, color, allowed, denied },profile_image_url,emote_ids},editor_in {id, display_name, login,role { id, name, position, color, allowed, denied },profile_image_url,emote_ids},follower_count,broadcast {type,title,game_name,viewer_count,},twitch_id,broadcaster_type,profile_image_url,created_at,emote_slots,audit_entries {id,type,timestamp,action_user_id,action_user {id, display_name, login,role { id, name, position, color, allowed, denied },profile_image_url,emote_ids},changes {key, values},target {type,data,id},reason}}",
                 "variables": {
-                    "id": "dontaddthisbot"
+                    "id": `${message.senderChannel}`
                 }
             }
         })
