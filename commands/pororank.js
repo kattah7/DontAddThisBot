b79c5be7ef47ef563627e74f53ef418a122305cf
kattah <kattahpro@gmail.com>
2022-06-25 05:47:25 +0900

    yo

--- commands/pororank.js
+++ commands/pororank.js
@@ -1,6 +1,8 @@
+const got = require("got");
+
 module.exports = {
     name: "rank",
-    cooldown: 1000,
+    cooldown: 10000,
     aliases: ["pororank"],
     description: "Check your rank in the poro leaderboard",
     poro: true,
@@ -15,18 +17,30 @@ module.exports = {
             }
         }
         const targetUser = args[0].toLowerCase()
+
+        const {banned, banphrase_data} = await got.post(`https://forsen.tv/api/v1/banphrases/test `, {json: {'message': targetUser}}).json();
+        console.log(banned, banphrase_data)
         const poroData = await bot.DB.poroCount.find({}).exec();
 
         const sorted = poroData.sort((a, b) => b.poroCount - a.poroCount);
+
         const kekw = sorted.slice(0, 5000000);
-        if (kekw.findIndex((user) => user.username == targetUser) + 1 == 0) {
+
+        if (banned == false) {
+            if (kekw.findIndex((user) => user.username == targetUser) + 1 == 0) {
+                return {
+                    text: `${targetUser} not found in database PoroSad`
+                }
+            }
             return {
-                text: `${targetUser} not found in database PoroSad`
+                text: `${targetUser} is rank #${kekw.findIndex((user) => user.username == targetUser) + 1} in the poro leaderboard! kattahBoom`,
+            }
+        } else if (banned == true) {
+            return {
+                text: `banned msg lol`
             }
         }
-        return {
-            text: `${targetUser} is rank ${kekw.findIndex((user) => user.username == targetUser) + 1} in the poro leaderboard! kattahBoom`,
-        }
+        
     
     }
 }
