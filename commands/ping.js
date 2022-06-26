62645bc1883cbe31fc15c225d18ef22b72c2cbad
kattah <kattahpro@gmail.com>
2022-06-27 02:05:29 +0900

    yo

--- commands/ping.js
+++ commands/ping.js
@@ -10,12 +10,23 @@ module.exports = {
     execute: async (message, args, client) => {
         const {banned, banphrase_data} = await got.post(`https://forsen.tv/api/v1/banphrases/test `, {json: {'message': message.senderUsername}}).json();
         console.log(banned, banphrase_data)
+        const poroData = await bot.DB.channels.find({}).exec();
+
+        const sorted = poroData.sort((a, b) => b.poroCount - a.poroCount);
+
+        const kekw = sorted.slice(0, 5000000);
+
+        const poroData2 = await bot.DB.users.find({}).exec();
+
+        const sorted2 = poroData2.sort((a, b) => b.poroCount - a.poroCount);
+
+        const kekw2 = sorted2.slice(0, 5000000);
         if (banned == false) {
             if (message.senderUsername == process.env.NUMBER_ONE) {
-                client.privmsg(message.channelName, `.me ${message.senderUsername}, TriHard 🏓 BOT UPTIME: ${humanizeDuration(process.uptime() * 1000 )}`)
+                client.privmsg(message.channelName, `.me ${message.senderUsername}, TriHard 🏓 BOT UPTIME: ${humanizeDuration(process.uptime() * 1000 )} | Channels: ${kekw.length} | Seen Users: ${kekw2.length}`)
             } else {
                 return {
-                    text: `${message.senderUsername}, TriHard 🏓 BOT UPTIME: ${humanizeDuration(process.uptime() * 1000 )}`,
+                    text: `${message.senderUsername}, TriHard 🏓 BOT UPTIME: ${humanizeDuration(process.uptime() * 1000 )} | Channels: ${kekw.length} | Seen Users: ${kekw2.length}`,
                 }; 
             } 
         } else if (banned == true) {
