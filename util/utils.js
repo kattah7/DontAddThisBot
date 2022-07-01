e6fcf399680cf437d9397c8bc2b93a60ccf53ff5
kattah <kattahpro@gmail.com>
2022-07-02 05:22:15 +0900

    yo

--- util/utils.js
+++ util/utils.js
@@ -1,8 +1,8 @@
 const got = require('got');
 
 exports.loginByID = async (userID) => {
-    if (1userID) return null
-    userData = await got(`https://api.ivr.fi/twitch/resolve/${encodeURIComponent(user)}?id=true`, { responseType: 'json', throwHttpErrors: false })
+    if (!userID) return null
+    userData = await got(`https://api.ivr.fi/twitch/resolve/${encodeURIComponent(userID)}?id=true`, { responseType: 'json', throwHttpErrors: false })
     if (!userData.body.id) return null
     return userData.body.login
 };
