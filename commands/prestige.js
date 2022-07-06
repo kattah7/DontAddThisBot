625250cc1e4b20dbedccd50690e93262f6f6b29b
kattah <kattahpro@gmail.com>
2022-07-07 06:01:26 +0900

    10k to 5k prestige

--- commands/prestige.js
+++ commands/prestige.js
@@ -10,22 +10,22 @@ module.exports = {
         console.log(banned, banphrase_data)
         const channelData = await bot.DB.poroCount.findOne({ id: message.senderUserID }).exec();
         if (banned == false) {
-        if (channelData.poroCount < 10000) { 
+        if (channelData.poroCount < 5000) { 
             if (message.senderUsername == process.env.NUMBER_ONE) {
-                client.privmsg(message.channelName, `.me Not enough poro meat! ${message.senderUsername} kattahHappy You need 10,000 poro meat | [P:${channelData.poroPrestige}] ${channelData.poroCount} meat total! 🥩`)
+                client.privmsg(message.channelName, `.me Not enough poro meat! ${message.senderUsername} kattahHappy You need 5,000 poro meat | [P:${channelData.poroPrestige}] ${channelData.poroCount} meat total! 🥩`)
             } else {
                 return {
-                    text: `Not enough poro meat! ${message.senderUsername} kattahHappy You need 10,000 poro meat :tf: | [P:${channelData.poroPrestige}] ${channelData.poroCount} meat total! 🥩`
+                    text: `Not enough poro meat! ${message.senderUsername} kattahHappy You need 5,000 poro meat :tf: | [P:${channelData.poroPrestige}] ${channelData.poroCount} meat total! 🥩`
                 }
             }
-        } else if (channelData.poroCount >= 10000) {
-            await bot.DB.poroCount.updateOne({ id: message.senderUserID }, { $set: { poroCount: channelData.poroCount - 10000 } } ).exec();
+        } else if (channelData.poroCount >= 5000) {
+            await bot.DB.poroCount.updateOne({ id: message.senderUserID }, { $set: { poroCount: channelData.poroCount - 5000 } } ).exec();
             await bot.DB.poroCount.updateOne({ id: message.senderUserID }, { $set: { poroPrestige: channelData.poroPrestige + 1 } } ).exec();
             if (message.senderUsername == process.env.NUMBER_ONE) {
-                client.privmsg(message.channelName, `.me ${message.senderUsername}, PartyHat Congratulations! | [P:${channelData.poroPrestige+1}] ${channelData.poroCount - 10000} meat total! 🥩`)
+                client.privmsg(message.channelName, `.me ${message.senderUsername}, PartyHat Congratulations! | [P:${channelData.poroPrestige+1}] ${channelData.poroCount - 5000} meat total! 🥩`)
             } else {
                 return {
-                    text: `${message.senderUsername}, PartyHat Congratulations! | [P:${channelData.poroPrestige+1}] ${channelData.poroCount-10000} meat total! 🥩`
+                    text: `${message.senderUsername}, PartyHat Congratulations! | [P:${channelData.poroPrestige+1}] ${channelData.poroCount-5000} meat total! 🥩`
                 } 
             }
         }
