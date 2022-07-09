3ffa64e025c1c85474e34b9fc0a23b2ba5f98f82
kattah <kattahpro@gmail.com>
2022-07-09 20:26:46 +0900

    new update

--- main.js
+++ main.js
@@ -67,6 +67,7 @@ client.on("PRIVMSG", async (message) => {
             client.say("kattah", `pokimane`)
         }
     }
+
     const channelData = await getChannel(message.channelName);
     const prefix = channelData.prefix ?? "|";
     if (!message.messageText.startsWith(prefix)) return;
@@ -81,19 +82,59 @@ client.on("PRIVMSG", async (message) => {
     try {
         if (command) {
             if (command.cooldown) {
+                const poroData = await bot.DB.poroCount.find({}).exec();
+
+                const sorted = poroData.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount);
+
+                const top1 = sorted.slice(0, 1);
+                const top2 = sorted.slice(1, 2);
+                const top3 = sorted.slice(2, 3);
+                const top4 = sorted.slice(3, 4);
+                const top5 = sorted.slice(4, 5);
+                const top6 = sorted.slice(5, 6);
+                const top7 = sorted.slice(6, 7);
+                const top8 = sorted.slice(7, 8);
+                const top9 = sorted.slice(8, 9);
+                const top10 = sorted.slice(9, 10);
+
+                const num1 = top1.map((user) => `${user.username}`)
+                const num2 = top2.map((user) => `${user.username}`)
+                const num3 = top3.map((user) => `${user.username}`)
+                const num4 = top4.map((user) => `${user.username}`)
+                const num5 = top5.map((user) => `${user.username}`)
+                const num6 = top6.map((user) => `${user.username}`)
+                const num7 = top7.map((user) => `${user.username}`)
+                const num8 = top8.map((user) => `${user.username}`)
+                const num9 = top9.map((user) => `${user.username}`)
+                const num10 = top10.map((user) => `${user.username}`)
                 if (userdata.level == 3 || userdata.level == 2) {
                     if (cooldown.has(`${command.name}${message.senderUserID}`)) return;
                     cooldown.set(`${command.name}${message.senderUserID}`, Date.now() + 10);
                     setTimeout(() => {
                         cooldown.delete(`${command.name}${message.senderUserID}`);
                     }, 10);
+                } else if (message.senderUsername == num1 || 
+                    message.senderUsername == num2 || 
+                    message.senderUsername == num3 || 
+                    message.senderUsername == num4 || 
+                    message.senderUsername == num5 || 
+                    message.senderUsername == num6 ||
+                    message.senderUsername == num7 ||
+                    message.senderUsername == num8 ||
+                    message.senderUsername == num9 ||
+                    message.senderUsername == num10){
+                    if (cooldown.has(`${command.name}${message.senderUserID}`)) return;
+                    cooldown.set(`${command.name}${message.senderUserID}`, Date.now() + 3000);
+                    setTimeout(() => {
+                        cooldown.delete(`${command.name}${message.senderUserID}`);
+                    }, 3000);
                 } else {
                     if (cooldown.has(`${command.name}${message.senderUserID}`)) return;
                     cooldown.set(`${command.name}${message.senderUserID}`, Date.now() + command.cooldown);
                     setTimeout(() => {
                         cooldown.delete(`${command.name}${message.senderUserID}`);
                     }, command.cooldown);
-                } 
+                 }
             }
             if (command.permission) {
                 if (command.permission == 1 && !message.isMod && message.channelName !== message.senderUsername) {
