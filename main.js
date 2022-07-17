3a1e575e63d24946aed6e22caf67e44fa71a3784
kattah <kattahpro@gmail.com>
2022-07-18 04:58:22 +0900

    yo

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ client.on("PRIVMSG", async (message) => {
             for (const lol of xd.split(" ")) 
             { client.say("kattah", `${lol}ping`) };
         }
-        if (message.senderUsername == "Spanixbot" && message.messageText.startsWith("to")) {
+        if (message.senderUsername == "spanixbot" && message.messageText.startsWith("to")) {
             console.log("ok")
             client.say("kattah", `to`)
         }
