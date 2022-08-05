33c4c061793cf9370c11c86fdcc6cf08c7432fa7
Kattah <kattahpro@gmail.com>
2022-08-05 09:49:44 +0900

    pls fix

--- api/routes/poros.js
+++ api/routes/poros.js
@@ -12,7 +12,7 @@ router.get('/lookup/:user', async (req, res) => {
 
     if (lastUsage) {
         const ms = new Date().getTime() + 1000 * 60 * 60 * 2 - new Date(lastUsage).getTime();
-        const divided = Math.abs(ms / 1000);
+        const divided = (ms / 1000).toFixed(3);
         res.json({
             cooldown: true,
             ms: divided,
