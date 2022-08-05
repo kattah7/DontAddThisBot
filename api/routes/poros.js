0e31b7fc83ee634de1b8dad9ce629ad50336ac06
Kattah <kattahpro@gmail.com>
2022-08-05 10:08:34 +0900

    pls fix

--- api/routes/poros.js
+++ api/routes/poros.js
@@ -8,14 +8,16 @@ router.get('/lookup/:user', async (req, res) => {
         return res.status(400).send('No user specified');
     }
 
-    const lastUsage = await bot.Redis.get(`poro:kattah`);
-    const ms = new Date(lastUsage).getTime() - new Date().getTime() + 1000 * 60 * 60 * 2;
+    const lastUsage = await bot.Redis.get(`poro:${req.params.user}`);
 
     if (lastUsage) {
-        res.json({
-            cooldown: true,
-            ms: humanizeDuration(ms),
-        });
+        if (new Date().getTime() - new Date(lastUsage).getTime() < 1000 * 60 * 60 * 2) {
+            const ms = new Date(lastUsage).getTime() - new Date().getTime() + 1000 * 60 * 60 * 2;
+            res.json({
+                cooldown: true,
+                ms: humanizeDuration(ms),
+            });
+        }
     } else {
         res.json({
             cooldown: false,
