d20fb71de28d5163fcd89d040ca86e1573da81a7
Kattah <kattahpro@gmail.com>
2022-08-07 06:57:05 +0900

    PoroOnly & OfflineOnly doesnt require level 3 to type it now

--- commands/offlineonly.js
+++ commands/offlineonly.js
@@ -2,19 +2,34 @@ module.exports = {
     name: "offlineonly",
     cooldown: 3000,
     description: "Make the bot only type when channel is offline",
-    level: 3,
+    permission: 2,
     poro: true,
     offline: true,
     execute: async(message, args, client) => {
-        if (args[0] == 'enable') {
-            await bot.DB.channels.updateOne({ username: message.channelName }, { $set: { offlineOnly: true } }).exec(); 
-            return {
-                text: `Commands are now only available only when ${message.channelName} is offline`
+        const user = await bot.DB.channels.findOne({ id: message.senderUserID }).exec();
+        if (user.offlineOnly) {
+            try {
+                await bot.DB.channels.updateOne({ id: message.senderUserID }, { $set: { offlineOnly: false } } ).exec();
+                return {
+                    text: `${message.senderUsername} is now online only`,
+                };
+            } catch (err) {
+                return {
+                    text: "Failed to update database",
+                };
             }
-        } else if (args[0] == 'disable') {
-            await bot.DB.channels.updateOne({ username: message.channelName }, { $set: { offlineOnly: false } }).exec(); 
-            return {
-                text: `All commands are now available in ${message.channelName}`
+            
+        }
+        if (!user.offlineOnly) {
+            try {
+                await bot.DB.channels.updateOne({ id: message.senderUserID }, { $set: { offlineOnly: true } } ).exec();
+                return {
+                    text: `${message.senderUsername} is now offline only`,
+                };
+            } catch (err) {
+                return {
+                    text: "Failed to update database",
+                };
             }
         }
         
--- commands/poroonly1.js
+++ commands/poroonly1.js
@@ -2,19 +2,34 @@ module.exports = {
     name: "poroonly",
     cooldown: 3000,
     description: "Make the bot only allow poro commands",
-    level: 3,
+    permission: 2,
     poro: true,
     offline: true,
     execute: async(message, args, client) => {
-        if (args[0] == 'enable') {
-            await bot.DB.channels.updateOne({ username: message.channelName }, { $set: { poroOnly: true } }).exec(); 
-            return {
-                text: `Only poro commands are now available in ${message.channelName}`
+        const user = await bot.DB.channels.findOne({ id: message.senderUserID }).exec();
+        if (user.poroOnly) {
+            try {
+                await bot.DB.channels.updateOne({ id: message.senderUserID }, { $set: { poroOnly: false } } ).exec();
+                return {
+                    text: `#${message.senderUsername} is now all cmds`,
+                };
+            } catch (err) {
+                return {
+                    text: "Failed to update database",
+                };
             }
-        } else if (args[0] == 'disable') {
-            await bot.DB.channels.updateOne({ username: message.channelName }, { $set: { poroOnly: false } }).exec(); 
-            return {
-                text: `All commands are now available in ${message.channelName}`
+            
+        }
+        if (!user.poroOnly) {
+            try {
+                await bot.DB.channels.updateOne({ id: message.senderUserID }, { $set: { poroOnly: true } } ).exec();
+                return {
+                    text: `#${message.senderUsername} is now poro cmds only`,
+                };
+            } catch (err) {
+                return {
+                    text: "Failed to update database",
+                };
             }
         }
         
