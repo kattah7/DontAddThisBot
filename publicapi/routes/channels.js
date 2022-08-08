d3d13da7d2f540885c5b1a597b5dac5d4b555c42
Kattah <kattahpro@gmail.com>
2022-08-08 18:28:48 +0900

    Added channelCount and totalPoros on API

--- publicapi/routes/channels.js
+++ publicapi/routes/channels.js
@@ -4,8 +4,15 @@ const router = express.Router();
 router.get('/api/bot/channels', async (req, res) => {
     const channels = await bot.DB.channels.find({}).exec();
     const mapped = channels.map(x => x.username)
+    const poroData = await bot.DB.poroCount.find({}).exec();
+    let sum = 0;
+    for (const xd of poroData) {
+        sum += xd.poroCount;
+    }
     return res.status(200).json({
+        channelCount: channels.length,
         channels: mapped,
+        totalPoros: sum,
     });
 })
 
