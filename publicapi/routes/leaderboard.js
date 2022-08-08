fa2e6d7326a168fad2829f545cb470ae4832366c
Kattah <kattahpro@gmail.com>
2022-08-08 18:29:04 +0900

    Added leaderboard API

---
+++ publicapi/routes/leaderboard.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/api/bot/leaderboard', async (req, res) => {
+    const poroData = await bot.DB.poroCount.find({}).exec();
+    const topUsers = poroData.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount).slice(0, 10).map((user) =>`[P:${user.poroPrestige}] ${user.username} - ${user.poroCount}`);
+
+    return res.status(200).json({
+        topUsers: topUsers,
+    });
+})
+
+module.exports = router;
