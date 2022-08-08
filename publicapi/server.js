9b6a1cece98537b2f7da297499b65efb88869c90
Kattah <kattahpro@gmail.com>
2022-08-08 18:28:20 +0900

    Added channels, leaderboard, porocount, userinfo API

--- publicapi/server.js
+++ publicapi/server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const channels = require('./routes/channels');
 const channelInfo = require('./routes/channelInfo');
+const channels = require('./routes/channels');
+const leaderboard = require('./routes/leaderboard');
+const poroCount = require('./routes/poroCount');
+const userInfo = require('./routes/users');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
-app.use(channels)
 app.use(channelInfo)
+app.use(channels)
+app.use(leaderboard)
+app.use(poroCount)
+app.use(userInfo)
 
 app.listen(3003, () => {
     console.log(`Public API is running on port 3003`);
