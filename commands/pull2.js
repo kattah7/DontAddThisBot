bbe9a39a72465559c50f4340b9edb9c8eaa60fd5
Kattah <kattahpro@gmail.com>
2022-08-09 18:05:24 +0900

    Added website pull command

---
+++ commands/pull2.js
@@ -0,0 +1,22 @@
+const { exec } = require("child_process");
+
+module.exports = {
+    name: "pull",
+    aliases: [],
+    cooldown: 3000,
+    level: 3,
+    description: "Pulls the latest commit from github. (Kattah only)",
+    execute: async (message, args, client) => {
+        await exec("cd /home/mywebsitexd && git reset --hard && git pull && npm i", (err) => {
+            if (err) {
+                console.error(err);
+                return {
+                    text: `FeelsDankMan !!! failed to pull commit`,
+                };
+            }
+        });
+
+        await client.say(message.channelName, "Commit pulled, restarting.. :)");
+        process.exit(0);
+    },
+};
