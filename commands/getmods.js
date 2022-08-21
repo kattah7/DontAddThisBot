ee10e548553ce2c2a6b7f85b38e62bc7285238ca
Kattah <kattahpro@gmail.com>
2022-08-22 05:19:16 +0900

    API Removed

--- commands/getmods.js
+++ commands/getmods.js
@@ -18,14 +18,39 @@ module.exports = {
                 throwHttpErrors: false,
                 responseType: 'json',
             })
-            const getArtist = await fetch(`https://api.poros.lol/api/twitch/artist/${targetUser}`, {
-                method: 'GET',
-            })
-            const getArtistJSON = await getArtist.json();
-            //console.log(getArtistJSON)
+            const userID = await utils.IDByLogin(targetUser);
+            const query = [];
+            query.push({
+                operationName: 'UserRolesCacheQuery',
+                variables: {
+                    channelID: userID,
+                    includeArtists: true,
+                    includeEditors: false,
+                    includeMods: false,
+                    includeVIPs: false,
+                },
+                extensions: {
+                    persistedQuery: {
+                        version: 1,
+                        sha256Hash: 'a0a9cd40e047b86927bf69b801e0a78745487e9560f3365fed7395e54ca82117',
+                    },
+                },
+            });
+
+            const { body: pogger } = await got.post('https://gql.twitch.tv/gql', {
+                throwHttpErrors: false,
+                responseType: 'json',
+                headers: {
+                    'Authorization': `OAuth ${process.env.TWITCH_GQL_TOKEN}`,
+                    'Client-Id': `${process.env.CLIENT_ID_FOR_GQL}`,
+                },
+                json: query,
+            });
+            const {artists} = pogger[0].data;
+            const artistsMapped = artists.edges.map(({node, grantedAt}) => ({id: node.id, login: node.login, displayName: node.displayName, grantedAt: grantedAt}));
             const modsMapped = getModsNVips.mods.map(x => x.login + " (" + x.grantedAt.split("T")[0] + ")" + " - " + "[MOD]")
             const vipsMapped = getModsNVips.vips.map(x => x.login + " (" + x.grantedAt.split("T")[0] + ")" + " - " + "[VIP]")
-            const artistMapped = getArtistJSON.artists.map(x => x.login + " (" + x.grantedAt.split("T")[0] + ")" + " - " + "[ARTIST]")
+            const artistMapped = artistsMapped.map(x => x.login + " (" + x.grantedAt.split("T")[0] + ")" + " - " + "[ARTIST]")
             const modsNvipsMapped = modsMapped.concat(vipsMapped, artistMapped)
             const { key } = await got
                 .post(`https://haste.fuchsty.com/documents`, {
--- publicapi/server.js
+++ publicapi/server.js
@@ -6,7 +6,7 @@ const channels = require('./routes/channels');
 const leaderboard = require('./routes/leaderboard');
 const poroCount = require('./routes/poroCount');
 const userInfo = require('./routes/users');
-const editors = require('./routes/editors');
+///const editors = require('./routes/editors');
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(channels)
 app.use(leaderboard)
 app.use(poroCount)
 app.use(userInfo)
-app.use(editors)
+//app.use(editors)
 
 app.listen(3003, () => {
     console.log(`Public API is running on port 3003`);
