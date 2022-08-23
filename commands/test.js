5c21b8d5e18e4d9e0346821e62d2a4b476f830f0
Kattah <kattahpro@gmail.com>
2022-08-24 04:05:56 +0900

    testing

--- commands/test.js
+++ commands/test.js
@@ -5,13 +5,6 @@ module.exports = {
     level: 3,
     cooldown: 5000,
     execute: async (message, args, client) => {
-        const url1 = `https://api.ivr.fi/twitch/subage/kattah/forsen`
-        const url2 = `https://api.ivr.fi/twitch/subage/turtoise/forsen`
-        const responses = await Promise.all([fetch(url1), fetch(url2), {
-            method: 'GET',
-        }])
         
-        const data = await Promise.all([responses[0].json(), responses[1].json()])
-        console.log(data[0])
     }
 }
