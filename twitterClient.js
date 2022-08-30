aaf24db07ef52d50cb11a7960ac90a3d2b3dd2bc
Kattah <kattahpro@gmail.com>
2022-08-30 18:47:21 +0900

    Inital Commit

---
+++ twitterClient.js
@@ -0,0 +1,12 @@
+const {TwitterApi} = require("twitter-api-v2");
+
+const client = new TwitterApi({
+    appKey: process.env.TWITTER_APP_KEY,
+    appSecret: process.env.TWITTER_APP_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACCESS_SECRET
+})
+
+const rwClient = client.readWrite
+
+module.exports = rwClient
