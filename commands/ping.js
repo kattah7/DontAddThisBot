bf58c35c70578d0d9f9f36a976d57773084967a0
Kattah <kattahpro@gmail.com>
2022-09-02 01:46:01 +0900

    fixed to isChannel rather than displaying all channels

--- commands/ping.js
+++ commands/ping.js
@@ -9,20 +9,12 @@ module.exports = {
     poro: true,
     stvOnly: true,
     execute: async (message, args, client) => {
-        const {banned, banphrase_data} = await got.post(`https://forsen.tv/api/v1/banphrases/test `, {json: {'message': message.senderUsername}}).json();
         //console.log(banned, banphrase_data)
 
-        const poroData = await bot.DB.channels.count({}).exec();
+        const poroData = await bot.DB.channels.count({ isChannel: true }).exec();
         const poroData2 = await bot.DB.users.count({}).exec();
-
-        if (banned == false) {
-            return {
-                text: `${message.senderUsername}, TriHard 🏓 BOT UPTIME: ${humanizeDuration(process.uptime() * 1000)} | Channels: ${poroData} | Seen Users: ${poroData2}`
-            }
-        } else if (banned == true) {
-            return {
-                text: `that msg is banned lol`
-            }
+        return {
+            text: `${message.senderUsername}, TriHard 🏓 BOT UPTIME: ${humanizeDuration(process.uptime() * 1000)} | Channels: ${poroData} | Seen Users: ${poroData2}`
         }
     },
 };
