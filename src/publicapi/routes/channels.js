6a7f05ce972355b212d142865ef4ce68a1a3b4f5
Kattah <kattahpro@gmail.com>
2022-09-09 11:15:03 +0900

    new shuffle

--- src/publicapi/routes/channels.js
+++ src/publicapi/routes/channels.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch')
-const utils = require('../../util/utils.js');
 
 router.get('/api/bot/channels', async (req, res) => {
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+    }
+
     const channels = await bot.DB.channels.find({ isChannel: true }).exec();
-    const length = channels.length;
-    utils.Shuffle(channels.username);
-    const mapped = channels.map(x => x.username)
+    shuffle(channels.username);
+    const mapped = channels.username.map(x => x)
     const randomSliced = mapped.splice(Math.floor(Math.random() * mapped.length), 100);
     const streams = await fetch(`https://api.twitch.tv/helix/streams?user_login=${randomSliced.join('&user_login=')}`, {
             headers: {
@@ -26,7 +42,7 @@ router.get('/api/bot/channels', async (req, res) => {
     };
 
     return res.status(200).json({
-        channelCount: length,
+        channelCount: channels.length,
         channels: mapped,
         totalPoros: sum,
         todaysCode: todaysCode.todaysCode,
