86fdba8999b36c836243e766cdd89a2d352e04e8
Kattah <kattahpro@gmail.com>
2022-09-13 04:10:52 +0900

    added if no args then reply with message to send

--- src/commands/massping.js
+++ src/commands/massping.js
@@ -1,18 +1,28 @@
-const got = require("got");
+const got = require('got');
 
 module.exports = {
-    name: "massping",
+    name: 'massping',
     aliases: [],
     cooldown: 3000,
     permission: 2,
-    description:":tf:",
-    botPerms: "vip",
+    description: ':tf:',
+    botPerms: 'vip',
     execute: async (message, args, client) => {
-        if (!/^[A-Z_\d]/i.test(args.join(" "))) return { text: `malformed text parameter` };
-        if (regex.racism.test(args.join(" "))) return { text: `🤨` };
+        if (!args[0]) {
+            return {
+                text: 'Please provide a message to send.',
+            };
+        }
+        if (!/^[A-Z_\d]/i.test(args.join(' '))) return { text: `malformed text parameter` };
+        if (regex.racism.test(args.join(' '))) return { text: `🤨` };
         const { chatters } = await got(`https://tmi.twitch.tv/group/user/${message.channelName}/chatters`).json();
-        for (const chatter of [...chatters.broadcaster, ...chatters.moderators, ...chatters.vips, ...chatters.viewers]) {
-            client.say(message.channelName, `${chatter} ${args.join(" ")}`);
+        for (const chatter of [
+            ...chatters.broadcaster,
+            ...chatters.moderators,
+            ...chatters.vips,
+            ...chatters.viewers,
+        ]) {
+            client.say(message.channelName, `${chatter} ${args.join(' ')}`);
         }
     },
 };
