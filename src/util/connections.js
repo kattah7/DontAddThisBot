a6767d318a345ac61e9ec2104c4a043a37994831
Kattah <kattahpro@gmail.com>
2022-09-24 23:13:34 +0900

    parallel connections

--- src/util/connections.js
+++ src/util/connections.js
@@ -1,15 +1,19 @@
-const { ChatClient, AlternateMessageModifier, SlowModeRateLimiter } = require("@kararty/dank-twitch-irc");
-require("dotenv").config();
+const { ChatClient, AlternateMessageModifier, SlowModeRateLimiter } = require('@kararty/dank-twitch-irc');
+require('dotenv').config();
 
 const client = new ChatClient({
     username: process.env.TWITCH_USERNAME,
     password: process.env.TWITCH_OAUTH,
     rateLimits: 'verifiedBot',
-    ignoreUnhandledPromiseRejections: true
+    ignoreUnhandledPromiseRejections: true,
+    connectionRateLimits: {
+        parallelConnections: 6,
+        releaseTime: 1000,
+    },
 });
 
 client.use(new AlternateMessageModifier(client));
 client.use(new SlowModeRateLimiter(client, 10));
-client.connect()
+client.connect();
 
 module.exports = { client };
