31a606fdb9d4fc67dca58f9e447390ba2f7bf51f
Kattah <kattahpro@gmail.com>
2022-09-27 23:03:12 +0900

    clear command

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,12 +1,21 @@
 module.exports = {
     name: 'clear',
     description: 'Clears the chat',
-    cooldown: 3000,
+    cooldown: 5000,
     permission: 1,
     botPerms: 'mod',
     async execute(message, args, client) {
-        for (let i = 0; i < 150; i++) {
-            client.privmsg(message.channelName, '/clear');
+        args[0]
+            ? isNaN(args[0])
+                ? await client.say(message.channelName, `Please put a valid nunmber :)`)
+                : args[0] > 100
+                ? await client.say(message.channelName, `You can only put less than 100 :)`)
+                : clearChat(args[0])
+            : clearChat(50);
+        function clearChat(amount) {
+            for (let i = 0; i < amount; i++) {
+                client.privmsg(message.channelName, `.clear`);
+            }
         }
     },
 };
