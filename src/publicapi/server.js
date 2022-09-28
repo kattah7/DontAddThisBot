cdaaac04f45c75d128fa706dc0f6c884a4b31b29
Kattah <kattahpro@gmail.com>
2022-09-29 03:42:32 +0900

    Added 4 endpoints for commands

--- src/publicapi/server.js
+++ src/publicapi/server.js
@@ -6,18 +6,24 @@ const channels = require('./routes/channels');
 const leaderboard = require('./routes/leaderboard');
 const poroCount = require('./routes/poroCount');
 const userInfo = require('./routes/users');
-///const editors = require('./routes/editors');
+const moderationCommands = require('./routes/commands/moderation');
+const stvCommands = require('./routes/commands/7tv');
+const poroCommands = require('./routes/commands/poro');
+const statsCommands = require('./routes/commands/stats');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
-app.use(channelInfo)
-app.use(channels)
-app.use(leaderboard)
-app.use(poroCount)
-app.use(userInfo)
-//app.use(editors)
+app.use(channelInfo);
+app.use(channels);
+app.use(leaderboard);
+app.use(poroCount);
+app.use(userInfo);
+app.use(moderationCommands);
+app.use(stvCommands);
+app.use(poroCommands);
+app.use(statsCommands);
 
 app.listen(3003, () => {
     Logger.info(`Public API is running on port 3003`);
