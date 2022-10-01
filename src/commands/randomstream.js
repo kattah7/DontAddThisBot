5aaff7442b1e8426846d07a3cd605c1a4d2024a5
Kattah <kattahpro@gmail.com>
2022-10-02 07:52:03 +0900

    From now on random stream without args[0]

--- src/commands/randomstream.js
+++ src/commands/randomstream.js
@@ -1,5 +1,5 @@
 const humanizeDuration = require('../util/humanizeDuration');
-const { GetGames, GetStreams } = require('../token/helix');
+const { GetFirstStreams, GetTopGames } = require('../token/helix');
 
 module.exports = {
     tags: 'stats',
@@ -8,24 +8,17 @@ module.exports = {
     cooldown: 5000,
     description: 'Fetches a random streamer from any game category',
     execute: async (message, args, client) => {
-        if (!args[0]) {
-            return {
-                text: `Pls write a category lol`,
-            };
-        }
+        const topgames = await GetTopGames(100);
+        const getRandomGame = topgames[Math.floor(Math.random() * topgames.length)];
+        const data2 = await GetFirstStreams(100, getRandomGame['id']);
+        const random = data2[Math.floor(Math.random() * data2.length)];
+        const { user_name, started_at, game_name, viewer_count, user_login, title } = random;
+        const ms = new Date().getTime() - Date.parse(started_at);
 
-        const { id } = (await GetGames(args.join(' ')))[0];
-        const data2 = await GetStreams(100, id);
-
-        var random = data2[Math.floor(Math.random() * data2.length)];
-        const ms = new Date().getTime() - Date.parse(random.started_at);
-
-        if (args[0].toLowerCase()) {
-            return {
-                text: `${random.user_name} been live for ${humanizeDuration(ms)} playing ${random.game_name} with ${
-                    random.viewer_count
-                } viewers. Title: ${random.title}. twitch.tv/${random.user_login} kattahSpin`,
-            };
-        }
+        return {
+            text: `${user_name} been live for ${humanizeDuration(
+                ms
+            )} playing ${game_name} with ${viewer_count} viewers. Title: ${title}. twitch.tv/${user_login} kattahSpin`,
+        };
     },
 };
