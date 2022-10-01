047409f4e9f08b428b5f30e8fc3466bfe55d6a7b
Kattah <kattahpro@gmail.com>
2022-10-02 07:51:50 +0900

    Fixed error when no channel

--- src/commands/clip.js
+++ src/commands/clip.js
@@ -10,7 +10,19 @@ module.exports = {
     execute: async (message, args, client) => {
         const targetChannel = await ParseUser(args[0] ?? message.channelName);
         const uid = await IDByLogin(targetChannel);
-        const { url, broadcaster_name, view_count, creator_name, created_at } = (await GetClips(uid))[0];
+        if (uid === null) {
+            return {
+                text: `Unknown channel`,
+            };
+        }
+        const getClip = (await GetClips(uid))[0];
+        if (!getClip) {
+            return {
+                text: `No clips found for ${targetChannel}`,
+            };
+        }
+
+        const { url, broadcaster_name, view_count, creator_name, created_at } = getClip;
         return {
             text: `${broadcaster_name}'s all time top clip with ${view_count} views by ${creator_name} at ${
                 created_at.split('T')[0]
