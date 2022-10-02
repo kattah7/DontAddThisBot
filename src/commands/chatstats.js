70cad6b706a12779c923a74d14d385b6d1818704
Kattah <kattahpro@gmail.com>
2022-10-02 09:28:00 +0900

    fix chatstats cmd

--- src/commands/chatstats.js
+++ src/commands/chatstats.js
@@ -1,4 +1,6 @@
 const got = require('got');
+const { ChatStats } = require('../token/SE.js');
+const { ParseUser } = require('../util/utils.js');
 
 module.exports = {
     tags: 'stats',
@@ -6,21 +8,20 @@ module.exports = {
     cooldown: 3000,
     description: 'Get channel emote, top chatter, top hastags and top command stats',
     execute: async (message, args, client) => {
-        const targetUser = args[0] ?? message.senderUsername;
-        let { body: userData, statusCode } = await got(
-            `https://api.streamelements.com/kappa/v2/chatstats/${targetUser}/stats`,
-            { timeout: 10000, throwHttpErrors: false, responseType: 'json' }
-        );
-        //console.log(userData);
-
-        if (userData.statusCode == 404) {
+        const targetUser = await ParseUser(args[0] ?? message.channelName);
+        const data = await ChatStats(targetUser);
+        if (data.error) {
             return {
-                text: `Channel doesn't exist :p maybe try using old name?`,
-            };
-        } else {
-            return {
-                text: `Total ${userData.totalMessages} MESSAGES, Top Chatter: ${userData.chatters[0].name} Amount: ${userData.chatters[0].amount}, Top BTTV Emote: ${userData.bttvEmotes[0].emote} used ${userData.bttvEmotes[0].amount} times, Top FFZ Emote: ${userData.ffzEmotes[0].emote} used ${userData.ffzEmotes[0].amount}, Top TWITCH Emote: ${userData.twitchEmotes[0].emote} used ${userData.twitchEmotes[0].amount}. Top Hastag: ${userData.hashtags[0].hashtag} used ${userData.hashtags[0].amount} times, Top command ${userData.commands[0].command} used ${userData.commands[0].amount} times :O`,
+                text: `⛔ ${data.message}`,
             };
         }
+        const { totalMessages, chatters, twitchEmotes } = data;
+        return {
+            text: `This channel has (${totalMessages.toLocaleString()}) messages in total; ${
+                chatters[0]['name']
+            } Top Chatter; "${twitchEmotes[0]['emote']}" Top Twitch Emote with (${twitchEmotes[0][
+                'amount'
+            ].toLocaleString()}) uses`,
+        };
     },
 };
