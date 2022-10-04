1a4c23931158fbb555d045ffd8a61b3206234928
Kattah <kattahpro@gmail.com>
2022-10-05 06:16:00 +0900

    ping kattah when error

--- src/util/discord.js
+++ src/util/discord.js
@@ -37,21 +37,22 @@ exports.NewPoro = async (AccAge, UID, username, channel, description, logo) => {
 };
 
 exports.racist = async (username, userID, channel, message) => {
-    const WebHook =`https://discord.com/api/webhooks/${process.env.RACIST}`;
+    const WebHook = `https://discord.com/api/webhooks/${process.env.RACIST}`;
     const WebhookMsg = {
         embeds: [
             {
-            color: 0x0099ff,
-            title: `Sent by ${username}(UID:${userID}) in #${channel}`,
-            author: {
+                color: 0x0099ff,
+                title: `Sent by ${username}(UID:${userID}) in #${channel}`,
+                author: {
                     name: 'racist detected',
                     con_url: 'https://i.nuuls.com/g8l2r.png',
                 },
-            description: `${message}`,
-            timestamp: new Date(),
-            footer: {
-                text: 'Pulled time',
-                icon_url:'https://static-cdn.jtvnw.net/jtv_user_pictures/89049925-b020-436f-bf9e-6262c0bc6419-profile_image-600x600.png ',
+                description: `${message}`,
+                timestamp: new Date(),
+                footer: {
+                    text: 'Pulled time',
+                    icon_url:
+                        'https://static-cdn.jtvnw.net/jtv_user_pictures/89049925-b020-436f-bf9e-6262c0bc6419-profile_image-600x600.png ',
                 },
             },
         ],
@@ -64,21 +65,22 @@ exports.racist = async (username, userID, channel, message) => {
 };
 
 exports.newChannel = async (username, userID, message) => {
-    const WebHook =`https://discord.com/api/webhooks/${process.env.NEW_CHANNEL}`;
+    const WebHook = `https://discord.com/api/webhooks/${process.env.NEW_CHANNEL}`;
     const WebhookMsg = {
         embeds: [
             {
-            color: 0x0099ff,
-            title: `Joined channel ${username}(UID:${userID})`,
-            author: {
+                color: 0x0099ff,
+                title: `Joined channel ${username}(UID:${userID})`,
+                author: {
                     name: 'New channel',
                     con_url: 'https://i.nuuls.com/g8l2r.png',
                 },
-            description: `${message}`,
-            timestamp: new Date(),
-            footer: {
-                text: 'Pulled time',
-                icon_url:'https://static-cdn.jtvnw.net/jtv_user_pictures/89049925-b020-436f-bf9e-6262c0bc6419-profile_image-600x600.png ',
+                description: `${message}`,
+                timestamp: new Date(),
+                footer: {
+                    text: 'Pulled time',
+                    icon_url:
+                        'https://static-cdn.jtvnw.net/jtv_user_pictures/89049925-b020-436f-bf9e-6262c0bc6419-profile_image-600x600.png ',
                 },
             },
         ],
@@ -91,21 +93,23 @@ exports.newChannel = async (username, userID, message) => {
 };
 
 exports.errorMessage = async (channel, username, message, error) => {
-    const WebHook =`https://discord.com/api/webhooks/${process.env.ERROR}`;
+    const WebHook = `https://discord.com/api/webhooks/${process.env.ERROR}`;
     const WebhookMsg = {
+        content: `<@363968088783323136>`,
         embeds: [
             {
-            color: 0x0099ff,
-            title: `Error in ${channel} by ${username}`,
-            author: {
+                color: 0x0099ff,
+                title: `Error in ${channel} by ${username}`,
+                author: {
                     name: 'Error',
                     con_url: 'https://i.nuuls.com/g8l2r.png',
                 },
-            description: `${message}, ${error}`,
-            timestamp: new Date(),
-            footer: {
-                text: 'Pulled time',
-                icon_url:'https://static-cdn.jtvnw.net/jtv_user_pictures/89049925-b020-436f-bf9e-6262c0bc6419-profile_image-600x600.png ',
+                description: `${message}, ${error}`,
+                timestamp: new Date(),
+                footer: {
+                    text: 'Pulled time',
+                    icon_url:
+                        'https://static-cdn.jtvnw.net/jtv_user_pictures/89049925-b020-436f-bf9e-6262c0bc6419-profile_image-600x600.png ',
                 },
             },
         ],
@@ -118,24 +122,25 @@ exports.errorMessage = async (channel, username, message, error) => {
 };
 
 exports.BAND = async (channel, action, message, color, pfp) => {
-    const WebHook =`https://discord.com/api/webhooks/${process.env.ACTION}`;
+    const WebHook = `https://discord.com/api/webhooks/${process.env.ACTION}`;
     const WebhookMsg = {
         embeds: [
             {
-            thumbnail: {
-                url: pfp
-            },
-            color: color,
-            title: `${action} in ${channel}`,
-            author: {
+                thumbnail: {
+                    url: pfp,
+                },
+                color: color,
+                title: `${action} in ${channel}`,
+                author: {
                     name: action,
                     con_url: 'https://i.nuuls.com/g8l2r.png',
                 },
-            description: `${message}`,
-            timestamp: new Date(),
-            footer: {
-                text: 'Pulled time',
-                icon_url:'https://static-cdn.jtvnw.net/jtv_user_pictures/89049925-b020-436f-bf9e-6262c0bc6419-profile_image-600x600.png ',
+                description: `${message}`,
+                timestamp: new Date(),
+                footer: {
+                    text: 'Pulled time',
+                    icon_url:
+                        'https://static-cdn.jtvnw.net/jtv_user_pictures/89049925-b020-436f-bf9e-6262c0bc6419-profile_image-600x600.png ',
                 },
             },
         ],
