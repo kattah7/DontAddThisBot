51c3996e9b99c1bf00ad5b53a238876b0cdd7ebb
Kattah <kattahpro@gmail.com>
2022-10-05 04:35:53 +0900

    poro metrics

--- src/apis/publicapi/routes/grafana.js
+++ src/apis/publicapi/routes/grafana.js
@@ -6,9 +6,14 @@ const register = new client.Registry();
 const channelsLengthGauge = new client.Gauge({
     name: 'channels',
     help: 'Number of channels',
-    registers: [register],
 });
-register.registerMetric(channelsLengthGauge);
+const totalPoros = new client.Gauge({
+    name: 'total_poros',
+    help: 'Total number of poros',
+});
+for (const metrics of [channelsLengthGauge, totalPoros]) {
+    register.registerMetric(metrics);
+}
 
 register.setDefaultLabels({
     app: 'DontAddThisBot',
@@ -18,7 +23,13 @@ client.collectDefaultMetrics({ register });
 
 setInterval(async () => {
     const channels = await bot.DB.channels.count({ isChannel: true }).exec();
+    const poroData = await bot.DB.poroCount.find({}).exec();
+    let sum = 0;
+    for (const xd of poroData) {
+        sum += xd.poroCount;
+    }
     channelsLengthGauge.set(channels);
+    totalPoros.set(sum);
 }, 100);
 
 router.get('/metrics', async (req, res) => {
