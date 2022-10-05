abc07e1305674a9d8e7f51f445192bc890905a6e
Kattah <kattahpro@gmail.com>
2022-10-05 23:06:55 +0900

    test

--- src/clients/JOIN.js
+++ src/clients/JOIN.js
@@ -1,12 +1,12 @@
 const { client } = require('../util/connections.js');
-const { createListener } = require('../util/pubSub.js');
+// const { createListener } = require('../util/pubSub.js');
 const { IDByLogin } = require('../util/utils.js');
 
 const JOIN = async function () {
     client.on('JOIN', async ({ channelName }) => {
         Logger.info(`Joined channel ${channelName}`);
         const channelID = await IDByLogin(channelName);
-        createListener([`790623318.${[channelID]}`], ['chat_moderator_actions']);
+        // createListener([`790623318.${[channelID]}`], ['chat_moderator_actions']);
     });
 };
 
