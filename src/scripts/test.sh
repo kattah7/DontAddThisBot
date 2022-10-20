1cfc5cb4abba96af81c546357e752e6486fa1615
Kattah <kattahpro@gmail.com>
2022-10-20 15:44:33 +0900

    bttv cmd

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -4,11 +4,11 @@ join() {
         -X GET \
         -o /dev/null \
         -w " %{http_code}\n" \
-        https://api.kattah.me/c/xqc)"
+        https://stats.kattah.me/api/bot/top)"
     echo "$1: $r"
 }
 
-for i in {0..10000}; do
+for i in {0..100}; do
     { join $i & } 3>&2 2>/dev/null
 done
 wait
