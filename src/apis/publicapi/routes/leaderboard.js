64c6d6fb2981fee1b18c1257ccb45c7816c6521b
Kattah <kattahpro@gmail.com>
2022-10-29 23:58:15 +0900

    update ranks

--- src/apis/publicapi/routes/leaderboard.js
+++ src/apis/publicapi/routes/leaderboard.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 router.get('/api/bot/leaderboard', async (req, res) => {
     const poroData = await bot.DB.poroCount.find({}).exec();
-    const topUsers = poroData.sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroCount - a.poroCount).slice(0, 10).map((user) =>`[P:${user.poroPrestige}] ${user.username} - ${user.poroCount}`);
+    const topUsers = poroData
+        .sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroRank - a.poroRank || b.poroCount - a.poroCount)
+        .slice(0, 10)
+        .map((user) => `[P:${user.poroPrestige}] ${user.username} - ${user.poroCount}`);
 
     return res.status(200).json({
         topUsers: topUsers,
     });
-})
+});
 
 module.exports = router;
