53ad944a346b0f1aa298b1948c932b7507dfcc44
Kattah <kattahpro@gmail.com>
2022-10-30 00:39:15 +0900

    test

---
+++ src/commands/test.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'test',
+    cooldown: 3000,
+    description: 'test',
+    level: 3,
+    async execute(message, args, client, userdata, params) {
+        const poroData = await bot.DB.poroCount.find({}).exec();
+        const topUsers = poroData.sort((a, b) => b.poroPrestige - a.poroPrestige);
+
+        const top5 = topUsers.slice(0, 5);
+        console.log(top5);
+    },
+};
