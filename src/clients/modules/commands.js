04e3ce0339873416326118fe60221864a548561c
Kattah <kattahpro@gmail.com>
2022-10-29 17:26:29 +0900

    PRIVMSG handler

---
+++ src/clients/modules/commands.js
@@ -0,0 +1,13 @@
+const { readdirSync } = require('fs');
+const commands = new Map();
+const aliases = new Map();
+
+exports.startCmds = async () => {
+    for (let file of readdirSync(`./src/commands/`).filter((file) => file.endsWith('.js'))) {
+        let pull = require(`../../commands/${file}`);
+        commands.set(pull.name, pull);
+        if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach((alias) => aliases.set(alias, pull.name));
+    }
+
+    return { commands, aliases };
+};
