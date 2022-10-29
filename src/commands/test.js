98038304eb3846837fb6f48500cc42e345711958
Kattah <kattahpro@gmail.com>
2022-10-29 18:46:50 +0900

    user update

--- src/commands/test.js
+++ src/commands/test.js
@@ -8,7 +8,11 @@ module.exports = {
     execute: async (message, args, client) => {
         const userDB = await bot.DB.channels.find({});
         for (const user of userDB) {
-            console.log(await loginByID(user.id));
+            const username = await loginByID(user.id);
+            if (user.username != username) {
+                await bot.DB.channels.findOneAndUpdate({ id: user.id }, { $set: { username: username } }).exec();
+                console.log(`Updated ${user.username} to ${username}`);
+            }
         }
     },
 };
