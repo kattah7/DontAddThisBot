f76247003f275679659a9d711e24b9c3da447c3d
Kattah <kattahpro@gmail.com>
2022-10-30 01:01:00 +0900

    fixed api

--- src/apis/publicapi/routes/poroCount.js
+++ src/apis/publicapi/routes/poroCount.js
@@ -22,7 +22,7 @@ router.get('/api/bot/porocount/:user', async (req, res) => {
     const poroData = await bot.DB.poroCount.find({}).exec();
     const userRank =
         poroData
-            .filter((a) => a.poroPrestige > 0)
+            .filter((a) => a.poroPrestige > -1)
             .sort((a, b) => b.poroPrestige - a.poroPrestige || b.poroRank - a.poroRank || b.poroCount - a.poroCount)
             .findIndex((user) => user.username == req.params.user) + 1;
     const poroLastUsage = await bot.Redis.get(`poro:${UID}`);
