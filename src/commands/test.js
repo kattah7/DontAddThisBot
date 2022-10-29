c74a48d9f28265ac8d6973ac2f76eea56a636689
Kattah <kattahpro@gmail.com>
2022-10-30 00:41:00 +0900

    mongo???

--- src/commands/test.js
+++ src/commands/test.js
@@ -4,10 +4,10 @@ module.exports = {
     description: 'test',
     level: 3,
     async execute(message, args, client, userdata, params) {
-        const poroData = await bot.DB.poroCount.find({}).exec();
+        const poroData = await bot.DB.poroCount.find().exec();
         const topUsers = poroData.sort((a, b) => b.poroPrestige - a.poroPrestige);
 
-        const top5 = topUsers.slice(-1, 5);
+        const top5 = topUsers.slice(0, 5);
         console.log(top5);
     },
 };
