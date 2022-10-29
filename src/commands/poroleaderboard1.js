86d4699243b984a95c12f2abfd4d9cf424083f80
Kattah <kattahpro@gmail.com>
2022-10-30 00:21:41 +0900

    asd

--- src/commands/poroleaderboard1.js
+++ src/commands/poroleaderboard1.js
@@ -19,7 +19,7 @@ module.exports = {
         const poroData = await bot.DB.poroCount.find({}).exec();
         // sort porodata by highest poroprestige and poro rank
         const topUsers = poroData.sort(
-            (a, b) => b.poroPrestige - a.poroPrestige || b.poroRank - a.poroRank > b.poroCount - a.poroCount
+            (a, b) => b.poroPrestige - a.poroPrestige || b.poroRank - a.poroRank < b.poroCount - a.poroCount
         );
 
         const top5 = topUsers.slice(0, 5);
