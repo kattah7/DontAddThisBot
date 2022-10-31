9016e077272ad2845aba9e2a5446b1f4a217af71
Kattah <kattahpro@gmail.com>
2022-10-31 23:46:12 +0900

    seprate commands

--- joiner/main.js
+++ joiner/main.js
@@ -3,19 +3,15 @@ const { PART } = require('./clients/PART');
 const { joinChannels } = require('./clients/channels');
 const sql = require('./data/db');
 
-async function joiner() {
-    const showTables = async () => {
-        // create a new database table that stores with json objects and arrays
-        await sql.query(
-            `CREATE TABLE IF NOT EXISTS channels (id SERIAL PRIMARY KEY, username TEXT NOT NULL, channelName JSONB NOT NULL)`
-        );
-    };
+const showTables = async () => {
+    // create a new database table that stores with json objects and arrays
+    await sql.query(
+        `CREATE TABLE IF NOT EXISTS channels (id SERIAL PRIMARY KEY, username TEXT NOT NULL, channelName JSONB NOT NULL)`
+    );
+};
 
-    showTables();
+showTables();
 
-    for (const client of [JOIN, PART, joinChannels]) {
-        client();
-    }
+for (const client of [JOIN, PART, joinChannels]) {
+    client();
 }
-
-module.exports = { joiner };
