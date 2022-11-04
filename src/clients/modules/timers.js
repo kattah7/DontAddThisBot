7e28e0b615e995c8916acc3806b2889431377be3
Kattah <kattahpro@gmail.com>
2022-11-05 05:01:05 +0900

    if error delete redis key

--- src/clients/modules/timers.js
+++ src/clients/modules/timers.js
@@ -25,43 +25,48 @@ const getTimers = () => {
                     Date: reminder_date,
                     current_price,
                 } = await JSON.parse(removeAllSlashes)[0];
-                const { data } = await fetch(
-                    `https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=${id}&page_num=1&sort_by=default&mode=&allow_tradable_cooldown=1&_=1667085434023`,
-                    {
-                        headers: {
-                            'accept': 'application/json, text/plain, */*',
-                            'accept-language': 'en-US,en;q=0.9',
-                            'sec-fetch-dest': 'empty',
-                            'sec-fetch-mode': 'cors',
-                            'sec-fetch-site': 'same-site',
-                            'x-requested-with': 'XMLHttpRequest',
-                        },
+                try {
+                    const { data } = await fetch(
+                        `https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=${id}&page_num=1&sort_by=default&mode=&allow_tradable_cooldown=1&_=1667085434023`,
+                        {
+                            headers: {
+                                'accept': 'application/json, text/plain, */*',
+                                'accept-language': 'en-US,en;q=0.9',
+                                'sec-fetch-dest': 'empty',
+                                'sec-fetch-mode': 'cors',
+                                'sec-fetch-site': 'same-site',
+                                'x-requested-with': 'XMLHttpRequest',
+                            },
+                        }
+                    ).then((res) => res.json());
+                    const { goods_infos, items } = data;
+                    const channelName = await loginByID(channel);
+                    const requestedUser = await loginByID(key.split(':')[1]);
+                    const readableDate = humanizeDuration(Date.now() - reminder_date);
+
+                    if (desired_price >= items[0].price) {
+                        await deleteRedisReminder(key);
+                        return await client.say(
+                            channelName,
+                            `@${requestedUser} The price of "${goods_infos[id].market_hash_name}" is now ${items[0].price} RMB! Your desired price was ${desired_price} (${readableDate} ago)`
+                        );
                     }
-                ).then((res) => res.json());
-                const { goods_infos, items } = data;
-                const channelName = await loginByID(channel);
-                const requestedUser = await loginByID(key.split(':')[1]);
-                const readableDate = humanizeDuration(Date.now() - reminder_date);
 
-                if (desired_price >= items[0].price) {
+                    if (current_price !== items[0].price && !desired_price) {
+                        await deleteRedisReminder(key);
+                        return await client.say(
+                            channelName,
+                            `@${requestedUser} The price of "${goods_infos[id].market_hash_name}" has changed from ${current_price} to ${items[0].price} RMB! (${readableDate} ago)`
+                        );
+                    }
+                } catch (error) {
+                    console.log(error);
                     await deleteRedisReminder(key);
-                    return await client.say(
-                        channelName,
-                        `@${requestedUser} The price of "${goods_infos[id].market_hash_name}" is now ${items[0].price} RMB! Your desired price was ${desired_price} (${readableDate} ago)`
-                    );
-                }
-
-                if (current_price !== items[0].price && !desired_price) {
-                    await deleteRedisReminder(key);
-                    return await client.say(
-                        channelName,
-                        `@${requestedUser} The price of "${goods_infos[id].market_hash_name}" has changed from ${current_price} to ${items[0].price} RMB! (${readableDate} ago)`
-                    );
                 }
             });
         };
         getRedisKeysAndValues();
-    }, 5000);
+    }, 25000);
 };
 
 module.exports = { getTimers };
