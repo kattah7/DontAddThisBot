a608f7f07b25649b6f007e399c3e68c86e866114
Kattah <kattahpro@gmail.com>
2022-11-22 16:52:49 +0900

    updated commands link

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -5,7 +5,7 @@ module.exports = {
     cooldown: 3000,
     execute: async (message, args, client) => {
         return {
-            text: `${message.senderUsername}, https://poros.lol/commands WutFace`,
+            text: `${message.senderUsername}, https://docs.poros.lol/global-commands WutFace`,
         };
     },
 };
