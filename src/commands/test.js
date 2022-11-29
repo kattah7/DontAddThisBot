0f93925b05cc4818c085f9f4f0847d9e05e94d8d
Kattah <kattahpro@gmail.com>
2022-11-29 18:08:05 +0900

    Remove users

--- src/commands/test.js
+++ src/commands/test.js
@@ -6,16 +6,12 @@ module.exports = {
     description: 'test',
     level: 3,
     execute: async (message, args, client) => {
-        const userDB = await bot.DB.channels.find({}).exec();
+        const userDB = await bot.DB.users.find({}).exec();
         // in a loop remove all commandsUsed from db
         for (const user of userDB) {
-            // console log all commandsUsed array
-            console.log(user.commandsUsed);
-            // completely remove commandsUsed array
-            await bot.DB.channels.findOneAndUpdate({ id: user.id }, { $set: { commandsUsed: [] } }).exec();
-            Logger.info(`Removed commandsUsed from ${user.username}`);
-            // remove entire array
-            await bot.DB.channels.findOneAndUpdate({ id: user.id }, { $unset: { commandsUsed: [] } }).exec();
+            await bot.DB.users.findOneAndUpdate({ id: user.id }, { $set: { commandsUsed: [] } }).exec();
+            await bot.DB.users.findOneAndUpdate({ id: user.id }, { $unset: { commandsUsed: [] } }).exec();
+            console.log(`Removed commandsUsed from ${user.username}`);
         }
     },
 };
