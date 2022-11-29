9eb28613c4f1f6a618f3562bca758b1f5fd453ac
Kattah <kattahpro@gmail.com>
2022-11-29 19:59:24 +0900

    test

--- src/commands/test.js
+++ src/commands/test.js
@@ -4,12 +4,14 @@ module.exports = {
     aliases: ['test'],
     cooldown: 3000,
     description: 'test',
-    level: 3,
     execute: async (message, args, client) => {
         const userDB = await bot.DB.users.find({}).exec();
         // in a loop remove all commandsUsed from db
         for (const user of userDB) {
-            if (!user.commandsUsed) continue;
+            if (!user.commandsUsed || user.commandsUsed.length === 0) {
+                console.log(`no commandsUsed for ${user.username}`);
+                continue;
+            }
             await bot.DB.users.findOneAndUpdate({ id: user.id }, { $set: { commandsUsed: [] } }).exec();
             await bot.DB.users.findOneAndUpdate({ id: user.id }, { $unset: { commandsUsed: [] } }).exec();
             console.log(`Removed commandsUsed from ${user.username}`);
