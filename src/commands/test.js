88698e82d536380ce4ba34e4c72e4c004337f484
Kattah <kattahpro@gmail.com>
2022-11-29 19:56:18 +0900

    test

--- src/commands/test.js
+++ src/commands/test.js
@@ -9,6 +9,7 @@ module.exports = {
         const userDB = await bot.DB.users.find({}).exec();
         // in a loop remove all commandsUsed from db
         for (const user of userDB) {
+            if (!user.commandsUsed) continue;
             await bot.DB.users.findOneAndUpdate({ id: user.id }, { $set: { commandsUsed: [] } }).exec();
             await bot.DB.users.findOneAndUpdate({ id: user.id }, { $unset: { commandsUsed: [] } }).exec();
             console.log(`Removed commandsUsed from ${user.username}`);
