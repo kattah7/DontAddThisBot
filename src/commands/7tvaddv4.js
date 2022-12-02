bfda87324536bfb89216634e8cf673bf7fbdaeb4
Kattah <kattahpro@gmail.com>
2022-12-02 20:00:05 +0900

    Updated error handler

--- src/commands/7tvaddv4.js
+++ src/commands/7tvaddv4.js
@@ -43,6 +43,8 @@ module.exports = {
 
         const isHashTag = /#/.test(args);
         const SearchEmote = await SearchSTVEmote(isHashTag ? args[1] : args[0], isHashTag ? false : true);
+        const { data, errors } = SearchEmote;
+
         let findEmote;
         if (isHashTag) {
             if (/#/.test(args[0])) {
@@ -51,7 +53,7 @@ module.exports = {
                 };
             }
 
-            const xda = SearchEmote.data.emotes.items.map((x) => [x.id, x.name]);
+            const xda = data.emotes.items.map((x) => [x.id, x.name]);
             const findThatEmote = xda.find((x) => x[1] === args[0]);
             if (!findThatEmote) {
                 return {
@@ -61,14 +63,18 @@ module.exports = {
             findEmote = findThatEmote;
         }
 
-        if (SearchEmote.errors) {
+        if (errors) {
             return {
-                text: `⛔ ${SearchEmote.errors[0]?.extensions?.message}; Use the tags feature for accurate results, ${
+                text: `⛔ ${errors[0]?.extensions?.message}; Use the tags feature for accurate results, ${
                     channelInfo.prefix ?? `|`
                 }add (emote) (tags)`,
             };
+        } else if (data.emotes?.items?.length === 0) {
+            return {
+                text: `⛔ No emote found "${args[0]}", make sure its case sensitive or use the tags feature`,
+            };
         } else {
-            const { id, name } = SearchEmote.data.emotes?.items[0];
+            const { id, name } = data.emotes?.items[0];
             const addEmote = await AddSTVEmote(findEmote ? findEmote[0] : id, emote_set.id, isParams);
             if (addEmote.errors) {
                 return {
