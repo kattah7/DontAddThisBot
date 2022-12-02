2089b56973f6f82c9f7f4d2d0544be2a5ee30f89
Kattah <kattahpro@gmail.com>
2022-12-02 19:13:02 +0900

    remove useless functions

--- src/token/stvREST.js
+++ src/token/stvREST.js
@@ -20,26 +20,6 @@ exports.getUsernameByStvID = async (stvID) => {
     return getUsername.body;
 };
 
-exports.StvInformation = async (userID) => {
-    if (!userID) return null;
-    const nameData = await got(`https://7tv.io/v3/users/twitch/${encodeURIComponent(userID)}`, {
-        responseType: 'json',
-        throwHttpErrors: false,
-    });
-    if (!nameData.body.id) return null;
-    return nameData.body;
-};
-
-exports.StvEmoteInformation = async (emoteID) => {
-    if (!emoteID) return null;
-    const emote = await got(`https://7tv.io/v3/emotes/${encodeURIComponent(emoteID)}`, {
-        responseType: 'json',
-        throwHttpErrors: false,
-    });
-    if (emote.body.id === '000000000000000000000000') return null;
-    return emote.body;
-};
-
 exports.GetEmotes = async (emoteID) => {
     if (!emoteID) return null;
     const emote = await got(`https://7tv.io/v3/emotes/${encodeURIComponent(emoteID)}`, {
