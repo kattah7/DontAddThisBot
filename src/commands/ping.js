f21f2cb6a363e40464f5b16366b9738036dafeeb
Kattah <kattahpro@gmail.com>
2022-12-06 03:54:37 +0900

    New command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,3 +1,5 @@
+const Redis = require('ioredis');
+const redis = new Redis({});
 const humanizeDuration = require('../util/humanizeDuration');
 
 module.exports = {
@@ -9,12 +11,16 @@ module.exports = {
     poro: true,
     stvOnly: true,
     execute: async (message, args, client) => {
-        const poroData = await bot.DB.channels.count({ isChannel: true }).exec();
-        const poroData2 = await bot.DB.users.count({}).exec();
+        const redisValue = await redis.get('channelsEndpoint');
+        const { channelCount, totalPoros, executedCommands } = JSON.parse(redisValue);
+        const ping1 = performance.now();
+        await client.ping();
+        const ping2 = performance.now();
+        const actualPing = (ping2 - ping1).toFixed(2);
         return {
             text: `${message.senderUsername}, kattahPoro 🏓 BOT UPTIME: ${humanizeDuration(
                 process.uptime() * 1000
-            )} | Channels: ${poroData} | Seen Users: ${poroData2}`,
+            )} - PING: ${actualPing} - Channels: ${channelCount} - Executed Commands: ${executedCommands} - Total Poros: ${totalPoros}`,
         };
     },
 };
