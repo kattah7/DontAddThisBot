2d965d23747b6d916f0037d1de7528fe97195a14
Kattah <kattahpro@gmail.com>
2022-12-21 01:29:50 +0900

    Moved cooldown to new folder

---
+++ src/clients/modules/cooldown.js
@@ -0,0 +1,16 @@
+const cooldown = new Set();
+
+exports.set = (id, ttl) => {
+	cooldown.add(id);
+	setTimeout(() => {
+		this.delete(id);
+	}, ttl);
+};
+
+exports.delete = (id) => {
+	cooldown.delete(id);
+};
+
+exports.has = (id) => {
+	return cooldown.has(id);
+};
