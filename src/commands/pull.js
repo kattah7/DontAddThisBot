b9502304959b9db65efbbd1199dfaed5458f15c5
Kattah <kattahpro@gmail.com>
2023-01-08 14:29:20 +0900

    Added PSQL Dump

--- src/commands/pull.js
+++ src/commands/pull.js
@@ -1,4 +1,5 @@
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
+const { postgres } = require('../../config.json');
 
 module.exports = {
 	name: 'pull',
@@ -7,7 +8,7 @@ module.exports = {
 	kattah: true,
 	description: 'Pulls the latest commit from github. (Kattah only)',
 	execute: async (client, msg) => {
-		await exec('cd /home/DontAddThisBot && git reset --hard && git pull && yarn && mongodump --db=dontaddthisbot --out=dump/', (err) => {
+		exec('cd /home/DontAddThisBot && git reset --hard && git pull && yarn && mongodump --db=dontaddthisbot --out=dump/', (err) => {
 			if (err) {
 				console.error(err);
 				return {
@@ -15,6 +16,23 @@ module.exports = {
 					reply: true,
 				};
 			}
+		});
+
+		const cwd = process.cwd();
+		const path1 = `${cwd}/psqldump/dontaddthisbot/data.sql`;
+		const child1 = spawn('pg_dump', ['-U', 'postgres', '-h', 'localhost', '-d', 'dontaddthisbot', '-f', path1], {
+			env: { PGPASSWORD: postgres.password },
+		});
+		child1.stderr.on('data', (data) => {
+			console.error(`child stderr:\n${data}`);
+		});
+
+		const path2 = `${cwd}/psqldump/dontaddthisbot/data.tar`;
+		const child2 = spawn('pg_dump', ['--format=custom', '-U', 'postgres', '-h', 'localhost', '-d', 'dontaddthisbot', '-f', path2], {
+			env: { PGPASSWORD: postgres.password },
+		});
+		child2.stderr.on('data', (data) => {
+			console.error(`child stderr:\n${data}`);
 		});
 
 		await client.say(msg.channel.login, 'Commit pulled, restarting.. MrDestructoid');
