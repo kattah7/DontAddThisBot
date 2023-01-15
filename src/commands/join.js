e618788e7b7084fe1e2a8f9ac93373c375000597
Kattah <kattahpro@gmail.com>
2023-01-16 08:10:09 +0900

    Command update

--- src/commands/join.js
+++ src/commands/join.js
@@ -4,83 +4,9 @@ module.exports = {
 	cooldown: 3000,
 	description: 'Join channel command',
 	execute: async (client, msg) => {
-		const channelData = await bot.DB.channels.findOne({ id: msg.user.id }).exec();
-		const poroData = await bot.DB.poroCount.findOne({ id: msg.user.id }).exec();
-		if (channelData) {
-			if (channelData.isChannel) {
-				return { text: `Already in channel #${msg.user.login}` };
-			} else {
-				try {
-					await ReJoinChannel(msg.user.id);
-					await client.join(msg.user.login);
-					return {
-						text: `Re-Joining channel #${msg.user.login}`,
-						reply: false,
-					};
-				} catch (error) {
-					return {
-						text: `Error joining channel #${msg.user.login}`,
-						reply: false,
-					};
-				}
-			}
-		} else {
-			try {
-				await client.join(msg.user.login);
-				const Result = await JoinChannel(msg.user.login, msg.user.id);
-				return {
-					text: Result.text,
-					reply: false,
-				};
-			} catch (error) {
-				return { text: `Error joining channel #${msg.user.login}` };
-			}
-		}
-
-		async function ReJoinChannel(senderUserID) {
-			await bot.DB.channels
-				.findOneAndUpdate(
-					{
-						id: senderUserID,
-					},
-					{
-						$set: {
-							isChannel: true,
-						},
-					},
-				)
-				.exec();
-		}
-
-		async function JoinChannel(senderUsername, senderUserID) {
-			const newChannel = new bot.DB.channels({
-				username: senderUsername,
-				id: senderUserID,
-				joinedAt: Date.now(),
-				isChannel: true,
-			});
-
-			await newChannel.save();
-			await client.say(senderUsername, `Joined channel, ${senderUsername} kattahPoro Check the bot's panels or https://docs.poros.lol for info!`);
-
-			if (poroData) {
-				await bot.DB.poroCount
-					.updateOne(
-						{ id: senderUserID },
-						{
-							$set: {
-								poroCount: poroData.poroCount + 100,
-							},
-						},
-					)
-					.exec();
-				return {
-					text: `Joined channel #${senderUsername} also gave u free 100 poros!!`,
-					reply: false,
-				};
-			} else {
-				return { text: `Joined channel #${senderUsername}`, reply: false };
-			}
-		}
+		return {
+			text: 'This command has been deprecated. Please use https://poros.lol',
+			reply: true,
+		};
 	},
 };
--- src/commands/part.js
+++ src/commands/part.js
@@ -6,42 +6,9 @@ module.exports = {
 	cooldown: 3000,
 	description: 'Part channel command',
 	execute: async (client, msg) => {
-		const { isChannel, addedBy } = await bot.DB.channels.findOne({ id: msg.channel.id }).exec();
-		if (isChannel && addedBy.length > 0) {
-			const { id, username, addedAt } = addedBy[0];
-			if (id !== msg.user.id && msg.user.id !== msg.channel.id) {
-				return {
-					text: `You cannot part the bot from this channel, since only the Broadcaster or the Moderator ${username} that added the bot (${humanizeDuration(new Date() - addedAt, {
-						largest: 3,
-					})}) ago can do that.`,
-				};
-			}
-
-			await bot.DB.channels.findOneAndUpdate({ id: msg.channel.id }, { $set: { isChannel: false } }).exec();
-			try {
-				await client.part(msg.channel.login);
-				return {
-					text: `Successfully parted from ${msg.channel.login}`,
-				};
-			} catch (err) {
-				return {
-					text: `Failed to part from ${msg.channel.login}`,
-				};
-			}
-		}
-
-		const channelData = await bot.DB.channels.findOneAndUpdate({ id: msg.user.id }, { $set: { isChannel: false } }).exec();
-		if (!channelData || !channelData.isChannel) {
-			return { text: `Not in channel #${msg.user.login}` };
-		}
-
-		if (channelData.isChannel) {
-			try {
-				await client.part(msg.user.login);
-				return { text: `Parting channel #${msg.user.login}` };
-			} catch (error) {
-				return { text: `Error leaving channel #${msg.user.login}` };
-			}
-		}
+		return {
+			text: 'This command has been deprecated. Please use https://poros.lol',
+			reply: true,
+		};
 	},
 };
