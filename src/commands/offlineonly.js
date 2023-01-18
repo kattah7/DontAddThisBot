fd443bca3b221ef97230d017b1e071f8aa955079
Kattah <kattahpro@gmail.com>
2023-01-19 07:33:56 +0900

    Command deprication

--- src/commands/offlineonly.js
+++ src/commands/offlineonly.js
@@ -7,47 +7,9 @@ module.exports = {
 	aliases: [],
 	offline: true,
 	execute: async (client, msg) => {
-		async function setStatus(channelID, Boolean) {
-			await bot.DB.channels
-				.updateOne(
-					{ id: channelID },
-					{
-						$set: {
-							offlineOnly: Boolean,
-						},
-					},
-				)
-				.exec();
-		}
-
-		const user = await bot.DB.channels.findOne({ id: msg.channel.id }).exec();
-		if (user?.offlineOnly) {
-			try {
-				await setStatus(msg.channel.id, false);
-				return {
-					text: `${msg.channel.login} is now online & offline only`,
-					reply: false,
-				};
-			} catch (err) {
-				return {
-					text: 'Failed to update database',
-					reply: false,
-				};
-			}
-		}
-		if (!user.offlineOnly) {
-			try {
-				await setStatus(msg.channel.id, true);
-				return {
-					text: `${msg.channel.login} is now offline only`,
-					reply: false,
-				};
-			} catch (err) {
-				return {
-					text: 'Failed to update database',
-					reply: false,
-				};
-			}
-		}
+		return {
+			text: `This command has been deprecated. Please use https://poros.lol/dashboard/${msg.channel.login}/settings`,
+			reply: true,
+		};
 	},
 };
