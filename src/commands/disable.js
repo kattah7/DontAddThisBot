571f7e2ae84d74ec9f11d02a418df4ebf43dbe66
Kattah <kattahpro@gmail.com>
2023-01-19 07:27:49 +0900

    Moved cmds

--- src/commands/disable.js
+++ src/commands/disable.js
@@ -8,93 +8,9 @@ module.exports = {
 	description: 'Disable a command',
 	permission: 1,
 	async execute(client, msg) {
-		if (!msg.args[0]) {
-			return {
-				text: `Please provide a command to disable.`,
-				reply: true,
-			};
-		}
-
-		const { commands } = await startCmds();
-		let namesAndAliases = {
-			command: '',
-			aliases: [],
+		return {
+			text: `This command has been deprecated. Please use https://poros.lol/dashboard/${msg.channel.login}/default`,
+			reply: true,
 		};
-
-		const input = msg.args[0].toLowerCase();
-		if (input === 'disable' || input === 'enable') {
-			return {
-				text: `You cannot disable the ${input === 'disable' ? 'disable' : 'enable'} command. 4Head`,
-				reply: true,
-			};
-		}
-
-		for (let command of commands) {
-			const { name, aliases, level } = command[1];
-			if (!aliases || level > 1) continue;
-			if (name === input) {
-				namesAndAliases.command = name;
-				namesAndAliases.aliases = aliases;
-				break;
-			} else if (aliases.includes(input)) {
-				namesAndAliases.command = name;
-				namesAndAliases.aliases = aliases;
-				break;
-			}
-		}
-
-		const { command, aliases } = namesAndAliases;
-		if (!command || command === '') {
-			return {
-				text: `This command does not exist.`,
-			};
-		}
-
-		const { rows } = await bot.SQL.query(`SELECT * FROM channel_settings WHERE twitch_id = '${msg.channel.id}' AND command = '${command}';`);
-
-		if (msg.command === 'disable') {
-			if (!rows[0] || rows[0].length === 0) {
-				await bot.SQL.query(
-					`INSERT INTO channel_settings (twitch_id, twitch_login, command, aliases, is_disabled) VALUES ('${msg.channel.id}', '${msg.user.login}', '${command}', '${JSON.stringify(
-						aliases,
-					)}', 1);`,
-				);
-
-				return {
-					text: `The command "${command}" has been disabled.`,
-					reply: true,
-				};
-			} else if (rows[0].is_disabled === 0) {
-				await bot.SQL.query(`UPDATE channel_settings SET is_disabled = 1 WHERE twitch_id = '${msg.channel.id}' AND command = '${command}';`);
-
-				return {
-					text: `The command "${command}" has been re-disabled.`,
-					reply: true,
-				};
-			}
-
-			if (rows[0].is_disabled === 1) {
-				return {
-					text: `The command "${command}" is already disabled.`,
-					reply: true,
-				};
-			}
-		} else if (msg.command === 'enable') {
-			if (rows.length === 0 || rows[0].is_disabled === 0) {
-				return {
-					text: `The command "${command}" is already enabled.`,
-					reply: true,
-				};
-			}
-
-			if (rows[0].is_disabled === 1) {
-				await bot.SQL.query(`UPDATE channel_settings SET is_disabled = 0 WHERE twitch_id = '${msg.channel.id}' AND command = '${command}';`);
-
-				return {
-					text: `The command "${command}" has been enabled.`,
-					reply: true,
-				};
-			}
-		}
 	},
 };
--- src/commands/setprefix.js
+++ src/commands/setprefix.js
@@ -7,13 +7,9 @@ module.exports = {
 	aliases: [],
 	usage: '<prefix>',
 	async execute(client, msg) {
-		if (!msg.args.length) return { text: `you need to specify the prefix`, reply: true };
-		const prefix = msg.args[0].toLowerCase();
-		if (prefix.length > 15) return { text: `prefix too long, the maximum length is 15 characters`, reply: true };
-		if (prefix === msg.prefix) return { text: `the channel prefix is already set to ${prefix}`, reply: true };
-		if (prefix.startsWith('.') || prefix.startsWith('/')) return { text: `the prefix was not set, this character is reserved for twitch commands` };
-
-		await bot.DB.channels.updateOne({ id: msg.channel.id }, { $set: { prefix: prefix } }).exec();
-		return { text: `prefix changed to "${prefix}" lol`, reply: true };
+		return {
+			text: `This command has been deprecated. Please use https://poros.lol/dashboard/${msg.channel.login}/settings`,
+			reply: true,
+		};
 	},
 };
