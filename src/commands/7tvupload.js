b7a5f45e821af5d63408d18ad5e69a64e7699446
Kattah <kattahpro@gmail.com>
2023-01-27 09:51:10 +0900

    Emote Command

---
+++ src/commands/7tvupload.js
@@ -0,0 +1,83 @@
+const sizeOf = require('buffer-image-size');
+const sharp = require('sharp');
+const isAnimated = require('is-animated');
+const { uploadEmote } = require('../token/stvREST.js');
+
+module.exports = {
+	tags: '7tv',
+	name: '7tvupload',
+	description: 'Upload an emote to 7TV!',
+	aliases: ['upload'],
+	cooldown: 3000,
+	permission: 2,
+	async execute(client, msg) {
+		if (!msg.args[0]) {
+			return {
+				text: 'Please provide a URL to an image!',
+				reply: true,
+			};
+		}
+
+		if (!msg.args[1]) {
+			return {
+				text: 'Please provide a name for the emote!',
+				reply: true,
+			};
+		}
+
+		if (!/^[a-zA-Z0-9]+$/.test(msg.args[1])) {
+			return {
+				text: 'Please provide an english name for the emote!',
+				reply: true,
+			};
+		}
+
+		const emoteLink = msg.args[0];
+		const maxSize = 7342592;
+		try {
+			const bufValue = await fetch(emoteLink).then((res) => res.arrayBuffer());
+			let Buffered = Buffer.from(bufValue);
+
+			const imageData = sizeOf(Buffered);
+			let dimension = [imageData.width, imageData.height];
+
+			let animated = isAnimated(Buffered);
+			const sharpOptions = { animated: animated };
+
+			if (Buffered.byteLength > maxSize || dimension[0] > 1000 || dimension[1] > 1000) {
+				await client.say(msg.channel.login, "Your emote is too big! I'll try to resize it for you, but it might not work. If it doesn't, please resize it yourself and try again.");
+				while (Buffered.byteLength > maxSize || dimension[0] > 1000 || dimension[1] > 1000) {
+					dimension = dimension.map((dimension) => Math.floor((dimension *= 0.7)));
+					const [x, y] = dimension;
+					const resizeOptions = {
+						width: x,
+						height: y,
+					};
+
+					console.log(resizeOptions, sharpOptions, Buffered.byteLength);
+					Buffered = await sharp(Buffered, sharpOptions).resize(resizeOptions).toBuffer();
+				}
+			}
+
+			const sender = msg.user.login;
+			const channel = msg.channel.login;
+			const upload = await uploadEmote(Buffered, msg.args[1], sender, channel);
+			if (upload.id) {
+				return {
+					text: `Successfully uploaded emote: https://7tv.app/emotes/${upload.id}`,
+					reply: true,
+				};
+			}
+
+			return {
+				text: `Error uploading emote to 7TV.`,
+				reply: true,
+			};
+		} catch (err) {
+			return {
+				text: 'Something went wrong! Please try again later.',
+				reply: true,
+			};
+		}
+	},
+};
--- src/token/stvREST.js
+++ src/token/stvREST.js
@@ -1,4 +1,5 @@
 const got = require('got');
+const { twitch } = require('../../config.json');
 
 exports.getUser = async (name) => {
 	if (!name) return null;
@@ -41,4 +42,31 @@ exports.GetEmotes = async (emoteID) => {
 
 exports.GlobalEmote = () => {
 	return '(7TV)';
+};
+
+exports.uploadEmote = async (bufValue, emoteName, senderName, channelName) => {
+	const data = {
+		name: emoteName,
+		flags: 0,
+		tags: ['uploaded', `${senderName}`, 'inchannel', `${channelName}`],
+	};
+
+	try {
+		const uploadEmote = await fetch(`https://7tv.io/v3/emotes`, {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer ${twitch.stv_token}`,
+				'Content-Type': 'YEAHBUTAVIF',
+				'X-Emote-Data': JSON.stringify(data),
+				'Content-Length': bufValue.byteLength.toString(10),
+			},
+			body: bufValue,
+		}).then((res) => res.json());
+		return uploadEmote;
+	} catch (err) {
+		return {
+			text: `Error uploading emote to 7TV`,
+			success: false,
+		};
+	}
 };
