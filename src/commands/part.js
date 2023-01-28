c4009c3ee51549053cf2db69e307b764b166ef1c
Kattah <kattahpro@gmail.com>
2023-01-28 21:17:06 +0900

    Added part command

--- src/commands/part.js
+++ src/commands/part.js
@@ -1,11 +1,23 @@
-const humanizeDuration = require('../misc/humanizeDuration');
-
 module.exports = {
 	name: 'part',
 	aliases: [],
 	cooldown: 3000,
 	description: 'Part channel command',
 	execute: async (client, msg) => {
+		const { addedBy, isChannel } = msg.mongoChannel;
+		if (addedBy.length > 0 && isChannel) {
+			const whoAdded = msg.mongoChannel.addedBy[0];
+			if (whoAdded.id === msg.user.id || whoAdded.id === msg.channel.id) {
+				await client.part(msg.channel.login);
+				await bot.DB.channels.findOneAndUpdate({ id: msg.channel.id }, { $set: { isChannel: false } }).exec();
+
+				return {
+					text: `I have left ${msg.channel.login} as you requested.`,
+					reply: true,
+				};
+			}
+		}
+
 		return {
 			text: 'This command has been deprecated. Please use https://poros.lol',
 			reply: true,
