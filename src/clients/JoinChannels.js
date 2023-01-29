6adaeafcdee8e200b6249dbe09530ede5d670bd6
Kattah <kattahpro@gmail.com>
2023-01-30 02:14:54 +0900

    Use redis

--- src/clients/JoinChannels.js
+++ src/clients/JoinChannels.js
@@ -1,9 +1,16 @@
 const { client } = require('../util/twitch/connections.js');
 
 const main = async () => {
+	const levelZeros = await bot.DB.users.find({ level: 0 }).exec();
+	for (const levelZero of levelZeros) {
+		await bot.Redis.set(`xd:kattah:banned:${levelZero.id}`, '1', 0);
+	}
+
 	let ChannelsArray = [];
 	const channels = await bot.DB.channels.find({ isChannel: true }).exec();
 	for (const channel of channels) {
+		const isBanned = await bot.Redis.get(`xd:kattah:banned:${channel.id}`);
+		if (isBanned === '1') continue;
 		ChannelsArray.push(channel.username);
 	}
 
