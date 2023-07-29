521cb0333faab2dfebedbfaa04f8784e83373eb9
Kattah <kattahpro@gmail.com>
2023-07-29 15:15:21 +0900

    lol

--- src/clients/events/USERNOTICE.js
+++ src/clients/events/USERNOTICE.js
@@ -11,13 +11,13 @@ const USERNOTICE = () => {
 		const { channelName, channelID, senderUsername, senderUserID } = msg;
 		if ((msg.isResub || msg.isSub || msg.AnonSubgift || msg.isAnonGiftPaidUpgrade || msg.isSubgift) && channelID === '137199626') {
 			try {
-				const res = await rewardPoros(senderUserID, 3500);
+				const res = await rewardPoros(senderUserID, 1000);
 				if (!res || res == null) {
 					await client.say('kattah', "You aren't registered! type |poro to get started");
 					return;
 				}
 
-				await client.say('kattah', `Thank you for subbing!, ${senderUsername}. Here is free 3,500 poros!. You have ${res.poroCount + 3500} poros now!`);
+				await client.say('kattah', `Thank you for subbing!, ${senderUsername}. Here is free 1,000 poros!. You have ${res.poroCount + 1000} poros now!`);
 			} catch (err) {
 				console.log(err);
 			}
